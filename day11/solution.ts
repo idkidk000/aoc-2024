1bff9d0f7fb7fee62a172dec18a5d032119522c9
adminadmin <git@akki.uk>
2024-12-11 14:37:07 +0000

    day11: typescript add class

--- day11/solution.ts
+++ day11/solution.ts
@@ -2,39 +2,44 @@
 
 const DEBUG = false;
 const EXAMPLE = false;
+const ITERATIONS = 75;
 
 const text = await Deno.readTextFile(EXAMPLE ? 'example.txt' : 'input.txt');
 if (DEBUG) console.debug({ text });
 
-let counts: Record<string, number> = {};
-text.split(/\s+/).forEach((value) => {
-  const prevCount = counts[value] ?? 0;
-  counts[value] = prevCount + 1;
-});
+class NumberCounter {
+  constructor() {}
+  private counts: Record<string, number> = {};
+  add(value: number | string, count: number) {
+    const valueString = typeof value === 'string' ? value : String(value);
+    if (DEBUG) console.debug({ value, valueString, count });
+    this.counts[valueString] = (this.counts[valueString] ?? 0) + count;
+  }
+  totalCount() {
+    return Object.values(this.counts).reduce((acc, count) => acc + count, 0);
+  }
+  entries() {
+    return Object.entries(this.counts);
+  }
+}
+
+let counts = new NumberCounter();
+text.split(/\s+/).forEach((value) => counts.add(value, 1));
 if (DEBUG) console.debug({ counts });
 
-for (let iteration = 0; iteration < 75; iteration++) {
-  const newCounts: Record<string, number> = {};
-  for (const [valueStr, count] of Object.entries(counts)) {
+for (let iteration = 0; iteration < ITERATIONS; iteration++) {
+  const newCounts = new NumberCounter();
+  for (const [valueStr, count] of counts.entries()) {
     if (valueStr === '0') {
-      const newValueStr = '1';
-      const prevCount = newCounts[newValueStr] ?? 0;
-      newCounts[newValueStr] = prevCount + count;
-      if (DEBUG) console.debug('zero', { valueStr, newValueStr, prevCount, count });
+      newCounts.add('1', count);
     } else if (valueStr.length % 2 == 0) {
-      for (const newValueStr of [valueStr.slice(0, valueStr.length / 2), String(parseInt(valueStr.slice(valueStr.length / 2)))]) {
-        const prevCount = newCounts[newValueStr] ?? 0;
-        newCounts[newValueStr] = prevCount + count;
-        if (DEBUG) console.debug('split', { valueStr, newValueStr, prevCount, count });
-      }
+      newCounts.add(valueStr.slice(0, valueStr.length / 2), count);
+      newCounts.add(parseInt(valueStr.slice(valueStr.length / 2)), count);
     } else {
-      const newValueStr = String(parseInt(valueStr) * 2024);
-      const prevCount = newCounts[newValueStr] ?? 0;
-      newCounts[newValueStr] = prevCount + count;
-      if (DEBUG) console.debug('mult', { valueStr, newValueStr, prevCount, count });
+      newCounts.add(parseInt(valueStr) * 2024, count);
     }
   }
-  const totalCount = Object.values(newCounts).reduce((acc, count) => acc + count, 0);
+  const totalCount = newCounts.totalCount();
   if (DEBUG) {
     console.debug({ iteration, counts, newCounts, totalCount });
   } else {
