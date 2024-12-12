02abdd1d02234c0d0a05e9f1e8b93cea4609cc07
adminadmin <git@akki.uk>
2024-12-12 19:11:14 +0000

    day 12: python part 2 and rewrite part 1

--- day12/solution.py
+++ day12/solution.py
@@ -1,82 +1,72 @@
 #!/usr/bin/env python3
-from collections import Counter
-import json
 
 DEBUG = True
+EXAMPLE = False
 
-with open('input.txt', 'r') as f:
-  text = f.read()
-if DEBUG: print(f'{text=}')
+with open('example.txt' if EXAMPLE else 'input.txt', 'r') as f:
+  grid = [list(x) for x in f.read().splitlines()]
+if DEBUG:
+  for x in grid:
+    print(f'{x}')
 
-# plant_types = {x for x in list(text) if x.strip()}
-# if DEBUG: print(f'{plant_types=}')
-grid = [list(x) for x in text.splitlines()]
-if DEBUG: print(f'{grid=}')
 row_count = len(grid)
 col_count = len(grid[0])
-if DEBUG: print(f'{row_count=} {col_count=}')
 
-# dict of coordinates by plot type (letter)
-plot_coords: dict[str, set[tuple[int, int]]] = {}
+# don't double-count walked coords
+walked = [[False] * col_count for _ in range(row_count)]
+
+
+def walk(row_ix: int, col_ix: int) -> tuple[int, int, set[tuple[int, int, int]]]:
+  walked[row_ix][col_ix] = True
+  area = 1
+  perimeter = 0
+  edges: set[tuple[int, int, int]] = set()
+  for dir_ix in range(4):
+    test_row_ix = row_ix + (-1 if dir_ix == 0 else 1 if dir_ix == 2 else 0)
+    test_col_ix = col_ix + (-1 if dir_ix == 1 else 1 if dir_ix == 3 else 0)
+    if 0 <= test_row_ix < row_count and 0 <= test_col_ix < col_count:
+      # in bounds
+      if grid[test_row_ix][test_col_ix] != grid[row_ix][col_ix]:
+        # a different area
+        perimeter += 1
+        edges.add((row_ix, col_ix, dir_ix))
+      elif not walked[test_row_ix][test_col_ix]:
+        # walk the connected area
+        new_area, new_perimeter, new_edges = walk(test_row_ix, test_col_ix)
+        perimeter += new_perimeter
+        area += new_area
+        edges.update(new_edges)
+    else:
+      # out of bounds
+      perimeter += 1
+      edges.add((row_ix, col_ix, dir_ix))
+  return (area, perimeter, edges)
+
+
+total_cost_1 = 0
+total_cost_2 = 0
 for row_ix in range(row_count):
   for col_ix in range(col_count):
-    char_at = grid[row_ix][col_ix]
-    if char_at not in plot_coords.keys():
-      plot_coords[char_at] = set()
-    plot_coords[char_at].add((row_ix, col_ix))
-# if DEBUG: print(f'{plot_coords=}')
-if DEBUG: print(f'plot_coords={json.dumps({k:sorted(str(x) for x in v) for k,v in plot_coords.items()},indent=2)}')
+    if not walked[row_ix][col_ix]:
+      area, perimeter, edges = walk(row_ix, col_ix)
+      cost_1 = area * perimeter
+      sides = 0
+      # convert the edges to distinct sides/faces
+      for edge in edges:
+        match edge[2]:
+          case 0 | 2:
+            # for up/down edges, count if there is not an edge to the left
+            if (edge[0], edge[1] - 1, edge[2]) not in edges:
+              sides += 1
+          case 1 | 3:
+            # for left/right edges, count if there is not an edge above
+            if (edge[0] - 1, edge[1], edge[2]) not in edges:
+              sides += 1
+      cost_2 = area * sides
+      char_at = grid[row_ix][col_ix]
+      print(f'{char_at=} {row_ix=} {col_ix=} {area=} {perimeter=} {sides=} {cost_1=} {cost_2=}')
+      total_cost_1 += cost_1
+      total_cost_2 += cost_2
 
-
-def map_connections(coord: tuple[int, int], result: set[tuple[int, int]]) -> set[tuple[int, int]]:
-  result.add(coord)
-  char_at = grid[coord[0]][coord[1]]
-  for dir_ix in range(4):
-    test_coord = (
-      coord[0] + (-1 if dir_ix == 0 else 1 if dir_ix == 2 else 0),
-      coord[1] + (-1 if dir_ix == 3 else 1 if dir_ix == 1 else 0),
-    )
-    if test_coord in plot_coords[char_at] and test_coord not in result:
-      result.update(map_connections(test_coord, result))
-  return result
-
-
-# dict of connected coordinates by origin (top-left) coordinate
-plot_origins: dict[tuple[int, int], set[tuple[int, int]]] = {}
-for plot_type in plot_coords.keys():
-  if DEBUG: print(f'{plot_type=} {plot_coords[plot_type]=}')
-  for plot_coord in plot_coords[plot_type]:
-    # every coord that this coord is connected to
-    connections = map_connections(plot_coord, set())
-    #top-left coord
-    origin = min(connections)
-    # we're duplicating a lot of work here so most likely we already have this exact data in plot_origins
-    if origin not in plot_origins:
-      plot_origins[origin] = connections
-    # if DEBUG: print(f'{origin=} {sorted(connections)=}')
-
-if DEBUG: print(f'{len(plot_origins)=} {plot_origins=} {len([y for x in plot_origins.values() for y in x])}')
-
-# area - length of plot_origins value for origin key
-
-plot_perimeters: Counter[tuple[int, int]] = Counter()
-for plot_origin, connections in plot_origins.items():
-  for connection in connections:
-    for dir_ix in range(4):
-      test_coord = (
-        connection[0] + (-1 if dir_ix == 0 else 1 if dir_ix == 2 else 0),
-        connection[1] + (-1 if dir_ix == 3 else 1 if dir_ix == 1 else 0),
-      )
-      if not (0 <= test_coord[0] < row_count and 0 <= test_coord[1] < row_count and test_coord in connections):
-        plot_perimeters[plot_origin] += 1
-if DEBUG: print(f'{plot_perimeters=}')
-
-total_price = 0
-for plot_origin, connections in plot_origins.items():
-  area = len(connections)
-  perimeter = plot_perimeters[plot_origin]
-  price = area * perimeter
-  if DEBUG: print(f'{plot_origin=} {area=} {perimeter=} {price=}')
-  total_price += price
-
-print(f'part 1: {total_price=}')
+print(f'part 1 {total_cost_1=}')
+print(f'part 2 {total_cost_2=}')
