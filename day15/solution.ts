6c6f3b1d02816e0ca4b480c5a2edf44bbd0b71a8
adminadmin <git@akki.uk>
2024-12-15 16:45:39 +0000

    day 15 part 2: typescript but there's a bug which is only present on input.txt

--- day15/solution.ts
+++ day15/solution.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env -S deno --allow-read
-// deno-lint-ignore-file no-case-declarations
+// deno-lint-ignore-file no-case-declarations no-fallthrough
 
 // const DEBUG = true;
 const DEBUG = false;
 // const FILENAME = 'example.txt';
 // const FILENAME = 'example2.txt';
+// const FILENAME = 'example3.txt';
+// const FILENAME = 'example4.txt';
 const FILENAME = 'input.txt';
 
 console.log(FILENAME);
@@ -99,4 +101,164 @@ const part1 = (mapText: string, moves: string[]) => {
   console.log('part 1:', gpsTotal);
 };
 
-part1(blocks[0], moves);
+const pushBoxCoords = (boxCoords: number[][], newCoords: number[][]) => {
+  //TODO: this is very inneficient. map of a coord class with a hash function would be faster
+  for (const newCoord of newCoords) {
+    let duplicate = false;
+    for (const boxCoord of boxCoords) {
+      if (boxCoord[0] == newCoord[0] && boxCoord[1] == newCoord[1]) {
+        duplicate = true;
+        break;
+      }
+    }
+    if (!duplicate) {
+      boxCoords.push(newCoord);
+    }
+  }
+  return boxCoords;
+};
+
+const walkVertBoxes = (
+  mapData: string[][],
+  row: number,
+  col: number,
+  offset: number
+): { boxCoords: number[][]; success: boolean } => {
+  let boxCoords: number[][] = [];
+  let success = true;
+  switch (mapData[row][col]) {
+    case '#':
+      success = false;
+      break;
+    case '[':
+      // possible BUG: this might add duplicate entries
+      const { boxCoords: llBoxCoords, success: llSuccess } = walkVertBoxes(mapData, row + offset, col, offset);
+      const { boxCoords: lrBoxCoords, success: lrSuccess } = walkVertBoxes(mapData, row + offset, col + 1, offset);
+      success = success && llSuccess && lrSuccess;
+      boxCoords = pushBoxCoords(boxCoords, [[row, col], [row, col + 1], ...llBoxCoords, ...lrBoxCoords]);
+      break;
+    case ']':
+      const { boxCoords: rlBoxCoords, success: rlSuccess } = walkVertBoxes(mapData, row + offset, col - 1, offset);
+      const { boxCoords: rrBoxCoords, success: rrSuccess } = walkVertBoxes(mapData, row + offset, col, offset);
+      success = success && rlSuccess && rrSuccess;
+      boxCoords = pushBoxCoords(boxCoords, [[row, col - 1], [row, col], ...rlBoxCoords, ...rrBoxCoords]);
+      break;
+    case '.':
+      success = true;
+      break;
+  }
+  for (const outer of boxCoords) {
+    let count = 0;
+    for (const inner of boxCoords) {
+      if (inner[0] == outer[0] && inner[1] == outer[1]) count++;
+    }
+    if (count > 1) throw new Error(`duplicate boxCoords ${outer} count=${count}`);
+  }
+  return { boxCoords, success };
+};
+
+const part2 = (mapText: string, moves: string[]) => {
+  const mapData = mapText
+    .split('\n')
+    .filter((line) => line.trim())
+    .map((line) =>
+      line
+        .split('')
+        .map((char) => {
+          switch (char) {
+            case '@':
+              return ['@', '.'];
+            case 'O':
+              return ['[', ']'];
+            default:
+              return [char, char];
+          }
+        })
+        .flat(1)
+    );
+  if (DEBUG) printMap(mapData);
+  const { rowCount, colCount, startRowIx, startColIx } = getMapParams(mapData);
+  let row = startRowIx;
+  let col = startColIx;
+  if (DEBUG) console.debug({ rowCount, colCount, row, col });
+  let moveId = 0;
+  for (const move of moves) {
+    moveId++;
+    const { offsetRow, offsetCol } = getOffset(move);
+    const newRow = row + offsetRow;
+    const newCol = col + offsetCol;
+    if (DEBUG) console.debug({ moveId, row, col, move, offsetRow, offsetCol, newRow, newCol });
+    const charAt = mapData[newRow][newCol];
+    switch (charAt) {
+      case '.':
+        mapData[row][col] = '.';
+        row = newRow;
+        col = newCol;
+        mapData[row][col] = '@';
+        break;
+      case '#':
+        break;
+      case '[':
+      case ']':
+        if (['<', '>'].includes(move)) {
+          // left/right
+          let boxCol = newCol;
+          // follow offsetCol until we find a non-box
+          while (['[', ']'].includes(mapData[newRow][boxCol])) {
+            boxCol += offsetCol;
+          }
+          if (DEBUG) console.debug('move left/right', { col, newCol, boxCol });
+          // if the non-box is empty
+          if (mapData[newRow][boxCol] == '.') {
+            // loop over newCol to boxCol and rewrite chars
+            for (let i = 1; i < Math.abs(newCol - boxCol) + 1; i++) {
+              // map i back to the correct column
+              const updateCol = newCol + i * offsetCol;
+              const updateChar = i % 2 == (offsetCol > 0 ? 1 : 0) ? '[' : ']';
+              if (DEBUG) console.debug('move left/right', { i, offsetCol, newCol, updateCol, updateChar });
+              // set char based on i mod 2
+              mapData[newRow][updateCol] = updateChar;
+            }
+            // move
+            mapData[row][col] = '.';
+            row = newRow;
+            col = newCol;
+            mapData[row][col] = '@';
+          }
+        } else {
+          // up down requires a recursive function
+          // return an array of coords as a number[] so sort works if we need it
+          const { boxCoords, success } = walkVertBoxes(mapData, newRow, newCol, offsetRow);
+          if (DEBUG) console.debug({ success, boxCoords });
+          if (success) {
+            if (boxCoords.length == 0) throw new Error('boxCoords cannot be empty');
+            // TODO: may need to sort depending on the walkVertBoxes function. would nbeed to order based ofn offsetRow
+            for (const [boxRow, boxCol] of boxCoords.toReversed()) {
+              if (DEBUG) console.debug({ boxRow, boxCol });
+              mapData[boxRow + offsetRow][boxCol] = mapData[boxRow][boxCol];
+              mapData[boxRow][boxCol] = '.';
+            }
+            // move
+            mapData[row][col] = '.';
+            row = newRow;
+            col = newCol;
+            mapData[row][col] = '@';
+          }
+        }
+
+        break;
+      default:
+        throw new Error(`invalid map char ${charAt} at ${newRow},${newCol}`);
+    }
+    if (DEBUG) printMap(mapData);
+    // if (moveId > 15) break;
+  }
+  const gpsTotal = mapData.reduce(
+    (rowAcc, row, rowIx) => (rowAcc += row.reduce((colAcc, char, colIx) => (colAcc += char == '[' ? rowIx * 100 + colIx : 0), 0)),
+    0
+  );
+  console.log('part 2:', gpsTotal);
+};
+
+// part1(blocks[0], moves);
+part2(blocks[0], moves);
