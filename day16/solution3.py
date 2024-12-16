6f9681888ce7a5ccfdc3528a2771f229276c3814
adminadmin <git@akki.uk>
2024-12-16 12:56:50 +0000

    day 16 part 2: python

--- day16/solution3.py
+++ day16/solution3.py
@@ -18,11 +18,16 @@ with open(FILENAME, 'r') as f:
 map_data = [list(x) for x in text.splitlines()]
 rows = len(map_data)
 cols = len(map_data[0])
-if DEBUG:
-  for r in range(rows):
-    print(f'''{r: 3d}  {''.join(map_data[r])}''')
-  print(f'{rows=} {cols=}')
 
+
+def draw_map(map_data: list[list[str]]):
+  if DEBUG:
+    for r in range(rows):
+      print(f'''{r: 3d}  {''.join(map_data[r])}''')
+    print(f'{rows=} {cols=}')
+
+
+draw_map(map_data)
 start_at = end_at = None
 for r in range(rows):
   for c in range(cols):
@@ -42,9 +47,9 @@ paths = [{
   'dir': 1,
 }]
 pos_costs = {}
+finished_paths = []
 for i in range(rows * cols):
   new_paths = []
-  count_done = 0
   for path in paths:
     for j in range(3):
       # turn on 0 and 2
@@ -64,10 +69,11 @@ for i in range(rows * cols):
         continue
       if pos in pos_costs:
         # discard on cheaper route to pos exists
-        if pos_costs[pos] <= cost:
+        # allow some extra turns for part 2. it slows things down by a couple of seconds
+        if pos_costs[pos] <= cost - 2001:
           continue
         else:
-          pos_costs[pos] = cost
+          pos_costs[pos] = min(pos_costs[pos], cost)
       else:
         pos_costs[pos] = cost
       new_path = {
@@ -77,13 +83,25 @@ for i in range(rows * cols):
         'pos': pos,
         'dir': direc,
       }
-      if new_path['done']:
-        count_done += 1
       new_paths.append(new_path)
   paths = new_paths
-  if count_done > 0:
-    done = [x for x in paths if x['done']]
-    cheapest = sorted(done, key=lambda x: x['cost'])[0]
-    print(f'{cheapest=}')
+  finished_paths.extend([x for x in paths if x['done']])
 
-    # break
+cheapest = sorted(finished_paths, key=lambda x: x['cost'])[0]
+if DEBUG: print(f'{cheapest=}')
+print(f'''part 1: {cheapest['cost']}''')
+
+# break
+
+best_paths = [x for x in finished_paths if x['cost'] == cheapest['cost']]
+if DEBUG: print(f'{best_paths=}')
+best_path_tiles = set()
+for best_path in best_paths:
+  best_path_tiles.update(best_path['hist'])
+if DEBUG: print(f'{best_path_tiles=}')
+
+map_data_2 = [list(x) for x in map_data]
+for best_path_tile in best_path_tiles:
+  map_data_2[best_path_tile[0]][best_path_tile[1]] = 'O'
+draw_map(map_data_2)
+print(f'part 2: {len(best_path_tiles)=}')
