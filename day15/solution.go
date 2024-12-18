d189ffc96494d2ba4f92a1dc6085d112cd68813d
adminadmin <git@akki.uk>
2024-12-18 21:56:50 +0000

    day 15 part 2: go but it has at least 1 bug

--- day15/solution.go
+++ day15/solution.go
@@ -26,7 +26,127 @@ func main() {
 	filename, debug := parseArgs()
 	fmt.Printf("filename: %s; debug: %d\n", filename, debug)
 	part1(filename, debug)
-	// part2(data)
+	part2(filename, debug)
+}
+
+func part2(filename string, debug uint8) {
+	mapData, moves, start := readData(filename, true)
+	if debug > 0 {
+		// fmt.Println(mapData)
+		drawMap(mapData)
+		// fmt.Println(moves)
+		fmt.Println("start:", start)
+	}
+	current := Coord{start.x, start.y}
+	offsets := map[uint8]Coord{
+		moveUp:    {0, -1},
+		moveRight: {1, 0},
+		moveDown:  {0, 1},
+		moveLeft:  {-1, 0},
+	}
+	isBox:=map[uint8]bool{
+		mapBoxLeft:true,
+		mapBoxRight:true,
+	}
+	writeBox:=[]uint8{
+		mapBoxLeft,
+		mapBoxRight,
+	}
+	for _, move := range moves {
+		offset := offsets[move]
+		testNext := Coord{current.x + offset.x, current.y + offset.y}
+		if debug > 1 {
+			fmt.Println("current:", current, "move:", move, "offset:", offset, "testNext", testNext)
+		}
+		switch mapData[testNext.y][testNext.x] {
+		case mapFree:
+			current = testNext
+		case mapWall:
+			break
+		case mapBoxLeft,mapBoxRight:
+			if move==moveLeft || move==moveRight {
+				/*
+					keep searching in offset until not box
+					rewrite the contents of everything between that point and us with box left or right based on distance+(0/1 direction offset)%2
+				*/
+				for isBox[mapData[testNext.y][testNext.x]] {
+					testNext.x+=offset.x
+				}
+				switch mapData[testNext.y][testNext.x] {
+				case mapFree:
+					//TODO: *think* i've squashed two off by ones here but need to test
+					for i:=min(current.x+offset.x,testNext.x);i<=max(current.x+offset.x,testNext.x);i++{
+						// coerce i from a fixed position to a distance, coerce offset.x from -1/1 to 0/1 then sum and uint mod 2 to get an index into the writeBox slice
+						//TODO: might need to reverse the offset -1/1 -> 0/1 to get the right index
+						mapData[current.y][i]=writeBox[(i-current.x+(offset.x+1)/2)&1]
+					}
+					mapData[current.y][current.x] = mapFree
+					current.x += offset.x
+					mapData[current.y][current.x] = mapFree
+				case mapWall:
+					break
+				}
+			} else {
+				boxesToMove:=[]Coord{{testNext.x,testNext.y}}
+				colsToResolve:=map[int]bool{testNext.x:true}
+				foundWall:=false
+				// set the initial states
+				if mapData[testNext.y][testNext.x]==mapBoxLeft{
+					boxesToMove = append(boxesToMove, Coord{testNext.x+1,testNext.y})
+					colsToResolve[testNext.x+1]=true
+				} else {
+					boxesToMove = append(boxesToMove, Coord{testNext.x-1,testNext.y})
+					colsToResolve[testNext.x-1]=true
+				}
+				// then loop until wall or free
+				for len(colsToResolve)>0 && !foundWall{
+					testNext.y+=offset.y
+					for x,_:=range colsToResolve{
+						switch mapData[testNext.y][x]{
+							case mapWall:
+								foundWall=true
+								continue
+							case mapFree:
+								delete(colsToResolve,x)
+							case mapBoxLeft:
+								colsToResolve[x+1]=true
+								boxesToMove = append(boxesToMove, Coord{testNext.y,x}, Coord{testNext.y,x+1})
+							case mapBoxRight:
+								colsToResolve[x-1]=true
+								boxesToMove = append(boxesToMove, Coord{testNext.y,x-1}, Coord{testNext.y,x})
+						}
+					}
+				}
+				if foundWall{
+					continue
+				}
+				// copy box map data to a map and set the old coords to mapFree
+				boxMapData:=map[Coord]uint8{}
+				for _,box:=range boxesToMove{
+					boxMapData[box]=mapData[box.y][box.x]
+					mapData[box.y][box.x]=mapFree
+				}
+				// loop over the copied data and write to mapData+offset.y
+				for box,val:=range boxMapData{
+					mapData[box.y+offset.y][box.x]=val
+				}
+				current.y+=offset.y
+			}
+		}
+	}
+	if debug > 0 {
+		drawMap(mapData)
+		fmt.Println("current:", current)
+	}
+	gpsSum:=0
+	for y,row:=range mapData{
+		for x,item:=range row{
+			if item==mapBoxLeft{
+				gpsSum+=y*100+x
+			}
+		}
+	}
+	fmt.Println("part 2:",gpsSum)
 }
 
 func part1(filename string, debug uint8) {
