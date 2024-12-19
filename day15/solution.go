d3dec44983efe9c1f57bf5c1f64bb4ee8827176e
adminadmin <git@akki.uk>
2024-12-19 12:56:32 +0000

    day 15: go formatting

--- day15/solution.go
+++ day15/solution.go
@@ -11,21 +11,22 @@ type Coord struct {
 	x, y int
 }
 
-const moveUp uint8 = 0
-const moveRight uint8 = 1
-const moveDown uint8 = 2
-const moveLeft uint8 = 3
-
-const mapFree uint8 = 0
-const mapWall uint8 = 1
-const mapBox uint8 = 2
-const mapBoxLeft uint8 = 3
-const mapBoxRight uint8 = 4
+const (
+	moveUp      uint8 = 0
+	moveRight   uint8 = 1
+	moveDown    uint8 = 2
+	moveLeft    uint8 = 3
+	mapFree     uint8 = 0
+	mapWall     uint8 = 1
+	mapBox      uint8 = 2
+	mapBoxLeft  uint8 = 3
+	mapBoxRight uint8 = 4
+)
 
 func main() {
 	filename, debug := parseArgs()
 	fmt.Printf("filename: %s; debug: %d\n", filename, debug)
-	// part1(filename, debug)
+	part1(filename, debug)
 	part2(filename, debug)
 }
 
@@ -33,7 +34,7 @@ func part2(filename string, debug uint8) {
 	mapData, moves, start := readData(filename, true)
 	if debug > 0 {
 		// fmt.Println(mapData)
-		drawMap(mapData,start)
+		drawMap(mapData, start)
 		// fmt.Println(moves)
 		fmt.Println("start:", start)
 	}
@@ -44,11 +45,11 @@ func part2(filename string, debug uint8) {
 		moveDown:  {0, 1},
 		moveLeft:  {-1, 0},
 	}
-	isBox:=map[uint8]bool{
-		mapBoxLeft:true,
-		mapBoxRight:true,
+	isBox := map[uint8]bool{
+		mapBoxLeft:  true,
+		mapBoxRight: true,
 	}
-	writeBox:=[]uint8{
+	writeBox := []uint8{
 		mapBoxLeft,
 		mapBoxRight,
 	}
@@ -63,114 +64,114 @@ func part2(filename string, debug uint8) {
 			current = testNext
 		case mapWall:
 			break
-		case mapBoxLeft,mapBoxRight:
-			if move==moveLeft || move==moveRight {
+		case mapBoxLeft, mapBoxRight:
+			if move == moveLeft || move == moveRight {
 				// keep searching in offset until not box
 				for isBox[mapData[testNext.y][testNext.x]] {
-					testNext.x+=offset.x
+					testNext.x += offset.x
 				}
 				switch mapData[testNext.y][testNext.x] {
 				case mapFree:
-					if debug>1{
-						fmt.Println("move:",move,"about to move boxes horizontally")
-						drawMap(mapData,current)
+					if debug > 1 {
+						fmt.Println("move:", move, "about to move boxes horizontally")
+						drawMap(mapData, current)
 					}
-					for i:=min(current.x+offset.x,testNext.x);i<=max(current.x+offset.x,testNext.x);i++{
+					for i := min(current.x+offset.x, testNext.x); i <= max(current.x+offset.x, testNext.x); i++ {
 						// coerce i from a fixed position to a distance, coerce offset.x from -1/1 to -1/0 then sum and uint mod 2 to get an index into the writeBox slice
-						mapData[current.y][i]=writeBox[(i-current.x+(offset.x-1)/2)&1]
+						mapData[current.y][i] = writeBox[(i-current.x+(offset.x-1)/2)&1]
 					}
 					mapData[current.y][current.x] = mapFree
 					current.x += offset.x
 					mapData[current.y][current.x] = mapFree
-					if debug>1{
+					if debug > 1 {
 						fmt.Println("moved boxes horizontally")
-						drawMap(mapData,current)
+						drawMap(mapData, current)
 					}
 				case mapWall:
 					break
 				}
 			} else {
 				// using maps as both of these will get dupes otherwise
-				boxesToMove:=map[Coord]bool{{testNext.x,testNext.y}:true}
-				colsToResolve:=map[int]bool{testNext.x:true}
-				foundWall:=false
+				boxesToMove := map[Coord]bool{{testNext.x, testNext.y}: true}
+				colsToResolve := map[int]bool{testNext.x: true}
+				foundWall := false
 				// set the initial states
-				if mapData[testNext.y][testNext.x]==mapBoxLeft{
-					boxesToMove[Coord{testNext.x+1,testNext.y}]=true
-					colsToResolve[testNext.x+1]=true
+				if mapData[testNext.y][testNext.x] == mapBoxLeft {
+					boxesToMove[Coord{testNext.x + 1, testNext.y}] = true
+					colsToResolve[testNext.x+1] = true
 				} else {
-					boxesToMove[Coord{testNext.x-1,testNext.y}]=true
-					colsToResolve[testNext.x-1]=true
+					boxesToMove[Coord{testNext.x - 1, testNext.y}] = true
+					colsToResolve[testNext.x-1] = true
 				}
 				// then loop until wall or free
-				for len(colsToResolve)>0 && !foundWall{
-					testNext.y+=offset.y
-					for x,_:=range colsToResolve{
-						switch mapData[testNext.y][x]{
-							case mapWall:
-								foundWall=true
-								continue
-							case mapFree:
-								delete(colsToResolve,x)
-							case mapBoxLeft:
-								boxesToMove[Coord{x,testNext.y}]=true
-								boxesToMove[Coord{x+1,testNext.y}]=true
-								colsToResolve[x+1]=true
-							case mapBoxRight:
-								boxesToMove[Coord{x-1,testNext.y}]=true
-								boxesToMove[Coord{x,testNext.y}]=true
-								colsToResolve[x-1]=true
+				for len(colsToResolve) > 0 && !foundWall {
+					testNext.y += offset.y
+					for x := range colsToResolve {
+						switch mapData[testNext.y][x] {
+						case mapWall:
+							foundWall = true
+							continue
+						case mapFree:
+							delete(colsToResolve, x)
+						case mapBoxLeft:
+							boxesToMove[Coord{x, testNext.y}] = true
+							boxesToMove[Coord{x + 1, testNext.y}] = true
+							colsToResolve[x+1] = true
+						case mapBoxRight:
+							boxesToMove[Coord{x - 1, testNext.y}] = true
+							boxesToMove[Coord{x, testNext.y}] = true
+							colsToResolve[x-1] = true
 						}
 					}
 				}
-				if foundWall{
+				if foundWall {
 					continue
 				}
 				// copy box map data to a map and set the old coords to mapFree
-				if debug>1{
-					fmt.Println("about to move boxes vertically",boxesToMove)
-					drawMap(mapData,current)
+				if debug > 1 {
+					fmt.Println("about to move boxes vertically", boxesToMove)
+					drawMap(mapData, current)
 				}
-				boxMapData:=map[Coord]uint8{}
-				for box:=range boxesToMove{
-					boxMapData[box]=mapData[box.y][box.x]
-					mapData[box.y][box.x]=mapFree
+				boxMapData := map[Coord]uint8{}
+				for box := range boxesToMove {
+					boxMapData[box] = mapData[box.y][box.x]
+					mapData[box.y][box.x] = mapFree
 				}
-				if debug>1{
-					fmt.Println("boxMapData:",boxMapData)
+				if debug > 1 {
+					fmt.Println("boxMapData:", boxMapData)
 				}
 				// loop over the copied data and write to mapData+offset.y
-				for box,val:=range boxMapData{
-					mapData[box.y+offset.y][box.x]=val
+				for box, val := range boxMapData {
+					mapData[box.y+offset.y][box.x] = val
 				}
-				current.y+=offset.y
-				if debug>1{
+				current.y += offset.y
+				if debug > 1 {
 					fmt.Println("moved boxes vertically")
-					drawMap(mapData,current)
+					drawMap(mapData, current)
 				}
 			}
 		}
 	}
 	if debug > 0 {
-		drawMap(mapData,current)
+		drawMap(mapData, current)
 		fmt.Println("current:", current)
 	}
-	gpsSum:=0
-	for y,row:=range mapData{
-		for x,item:=range row{
-			if item==mapBoxLeft{
-				gpsSum+=y*100+x
+	gpsSum := 0
+	for y, row := range mapData {
+		for x, item := range row {
+			if item == mapBoxLeft {
+				gpsSum += y*100 + x
 			}
 		}
 	}
-	fmt.Println("part 2:",gpsSum)
+	fmt.Println("part 2:", gpsSum)
 }
 
 func part1(filename string, debug uint8) {
 	mapData, moves, start := readData(filename, false)
 	if debug > 0 {
 		// fmt.Println(mapData)
-		drawMap(mapData,start)
+		drawMap(mapData, start)
 		// fmt.Println(moves)
 		fmt.Println("start:", start)
 	}
@@ -209,21 +210,21 @@ func part1(filename string, debug uint8) {
 		}
 	}
 	if debug > 0 {
-		drawMap(mapData,current)
+		drawMap(mapData, current)
 		fmt.Println("current:", current)
 	}
-	gpsSum:=0
-	for y,row:=range mapData{
-		for x,item:=range row{
-			if item==mapBox{
-				gpsSum+=y*100+x
+	gpsSum := 0
+	for y, row := range mapData {
+		for x, item := range row {
+			if item == mapBox {
+				gpsSum += y*100 + x
 			}
 		}
 	}
-	fmt.Println("part 1:",gpsSum)
+	fmt.Println("part 1:", gpsSum)
 }
 
-func drawMap(mapData [][]uint8,target Coord) {
+func drawMap(mapData [][]uint8, target Coord) {
 	mapObjects := map[uint8]string{
 		mapWall:     "#",
 		mapBox:      "O",
@@ -235,7 +236,7 @@ func drawMap(mapData [][]uint8,target Coord) {
 		line := make([]string, len(row)+1)
 		line[0] = fmt.Sprintf("% 3d:  ", y)
 		for x, char := range row {
-			if target.x==x && target.y==y {
+			if target.x == x && target.y == y {
 				line[x+1] = "@"
 			} else {
 				line[x+1] = mapObjects[char]
