4775f70f893f9647f56f9971e429a5fabe0e3015
adminadmin <git@akki.uk>
2024-12-21 20:01:30 +0000

    day 20 part 1: python fast

--- day20/solution.py
+++ day20/solution.py
@@ -40,64 +40,61 @@ def draw_map(map_data: list[list[str]]):
   print(f'{rows=} {cols=}')
 
 
-def get_paths():
-  paths = deque([[start_coord]])
-  completed_paths = []
-  while len(paths):
-    path = paths.popleft()
-    current = path[-1]
+def get_distances():
+  distances = [[-1] * cols for _ in range(rows)]
+  cr, cc = start_coord
+  distances[cr][cc] = 0
+  while (cr, cc) != end_coord:
+    if DEBUG > 1: print(f'get_distances {cr=} {cc=}')
     for d in D4:
-      test_coord = (
-        current[0] + d[0],
-        current[1] + d[1],
-      )
-      if test_coord in path: continue
-      if not (0 <= test_coord[0] < rows and 0 <= test_coord[1] < cols): continue
-      match map_data[test_coord[0]][test_coord[1]]:
-        case '.':
-          paths.append([*path, test_coord])
-        case 'E':
-          completed_paths.append([*path, test_coord])
-    if DEBUG > 1: print(f'{len(paths)=} {len(paths[0]) if len(paths) else None}')
-  return completed_paths
+      nr, nc = cr + d[0], cc + d[1]
+      if not (0 <= nr < rows and 0 <= nc < cols): continue
+      if distances[nr][nc] != -1: continue
+      if map_data[nr][nc] == '#': continue
+      distances[nr][nc] = distances[cr][cc] + 1
+      cr, cc = nr, nc
+      break
+  return distances
 
 
-draw_map(map_data)
-regular_paths = get_paths()
-print(f'{len(regular_paths)=}')
-assert len(regular_paths) == 1
-regular_path = regular_paths[0]
-regular_length = len(regular_path)
-print(f'{regular_length=}')
+def part1():
+  distances = get_distances()
+  # for debugging
+  shortcut_time_counts: dict[int, int] = {}
+  shortcut_count = 0
 
-shortcut_time_counts = {}
-shortcut_count = 0
-for i, coord in enumerate(regular_path):
-  for d in [
-    (-2, 0),
-    (-1, 1),
-    (0, 2),
-    (1, 1),
-    (2, 0),
-    (1, -1),
-    (0, -2),
-    (-1, -1),
-  ]:
-    test_coord = (
-      coord[0] + d[0],
-      coord[1] + d[1],
-    )
-    if not (0 <= test_coord[0] < rows and 0 <= test_coord[1] < cols): continue
-    if test_coord not in regular_path: continue
-    saving = regular_path.index(test_coord) - i - 2
-    if saving <= 0: continue
-    if saving not in shortcut_time_counts.keys():
-      shortcut_time_counts[saving] = 1
-    else:
-      shortcut_time_counts[saving] += 1
-    if saving < 100: continue
-    shortcut_count += 1
+  for r in range(rows):
+    for c in range(cols):
+      origin_distance = distances[r][c]
+      if origin_distance == -1: continue
+      for d in [
+        (-2, 0),
+        (-1, 1),
+        (0, 2),
+        (1, 1),
+        (2, 0),
+        (1, -1),
+        (0, -2),
+        (-1, -1),
+      ]:
+        nr, nc = r + d[0], c + d[1]
+        if not (0 <= nr < rows and 0 <= nc < cols): continue
+        distance = distances[nr][nc]
+        if distance == -1: continue
+        saving = distance - origin_distance - 2
+        if saving <= 0: continue
+        if saving not in shortcut_time_counts.keys():
+          shortcut_time_counts[saving] = 1
+        else:
+          shortcut_time_counts[saving] += 1
+        if saving < 100: continue
+        shortcut_count += 1
 
-for k in sorted(shortcut_time_counts.keys()):
-  print(f'shortcut count: {k=} v={shortcut_time_counts[k]}')
-print(f'part 1: {shortcut_count=}')
+  if DEBUG > 1:
+    for k in sorted(shortcut_time_counts.keys()):
+      print(f'shortcut count: {k=} v={shortcut_time_counts[k]}')
+  print(f'part 1: {shortcut_count=}')
+
+
+# draw_map(map_data)
+part1()
