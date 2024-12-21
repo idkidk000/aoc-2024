68368b30f66318ede007d54b4baa96a6ad50baa0
adminadmin <git@akki.uk>
2024-12-21 14:46:15 +0000

    day 21 part 1: python working but a bit slow

--- day21/solution.py
+++ day21/solution.py
@@ -6,7 +6,12 @@ from collections import deque
 sys.setrecursionlimit(1_000_000)
 DEBUG = 0
 FILENAME = 'example.txt'
-D4 = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+D4 = [
+  (-1, 0, '^'),
+  (0, 1, '>'),
+  (1, 0, 'v'),
+  (0, -1, '<'),
+]
 
 for arg in sys.argv[1:]:
   if arg == '-i': FILENAME = 'input.txt'
@@ -19,7 +24,7 @@ for arg in sys.argv[1:]:
 
 with open(FILENAME, 'r') as f:
   text = f.read()
-door_codes = [list(x) for x in text.splitlines()]
+door_codes = text.splitlines()
 if DEBUG > 0: print(f'{door_codes}')
 
 door_keypad = {
@@ -44,105 +49,104 @@ robot_keypad = {
   '>': (1, 2),
 }
 
-# def gen_possible_inputs(outputs: list[str], keypad: dict[str, tuple[int, int]]) -> set[tuple[str]]:
-#   prefixed_outputs = ['A', *outputs]
-#   rev_keypad = {v: k for k, v in keypad.items()}
-#   for from_btn, to_btn in zip(prefixed_outputs[:-1], prefixed_outputs[1:]):
-#     from_coord = keypad[from_btn]
-#     to_coord = keypad[to_btn]
-#     paths = deque([(from_coord,)])
-#     completed_paths: list[tuple[tuple[int, int]]] = []
-#     max_moves = abs(to_coord[0] - from_coord[0]) + to_coord[1] - from_coord[1]
-#     while len(paths):
-#       path = paths.pop()
-#       curr_coord = path[-1]
-#       for d in D4:
-#         test_coord = (
-#           curr_coord[0] + d[0],
-#           curr_coord[1] + d[1],
-#         )
-#         if test_coord in path: continue
-#         char_at = rev_keypad.get(to_coord)
-#         if char_at is None: continue
-#         next_path = (
-#           *path,
-#           test_coord,
-#         )
-#         print(f'{path=} {curr_coord=} {d=} {test_coord=} {next_path=}')
-#         if char_at == to_btn:
-#           completed_paths.append(next_path)
-#         else:
-#           paths.append(next_path)
-
-#   return False
+key_paths_cache: dict[tuple[str, str], list[str]] = {}
 
 
-def gen_robot_inputs_door(outputs: list[str]) -> list[str]:
-  # accepts door keypresses, returns robot keypresses
+def get_key_paths(btn_from: str, btn_to: str, keypad: dict[str, tuple[int, int]]):
+  cached = key_paths_cache.get((btn_from, btn_to))
+  if cached is not None: return cached
+  rev_keypad = {v: k for k, v in keypad.items()}
+  if btn_from == btn_to:
+    key_paths_cache[(btn_from, btn_to)] = ['A']
+  else:
+    key_paths_cache[(btn_from, btn_to)] = []
+    coord_from = keypad[btn_from]
+    coord_to = keypad[btn_to]
+    coord_delta = (
+      coord_to[0] - coord_from[0],
+      coord_to[1] - coord_from[1],
+    )
+    max_moves = abs(coord_delta[0]) + abs(coord_delta[1]) + 1
+    paths = deque([([(
+      *coord_from,
+      '',
+    )])])
+    while len(paths):
+      path = paths.pop()
+      coord_curr = path[-1]
+      for d in D4:
+        coord_next = (
+          coord_curr[0] + d[0],
+          coord_curr[1] + d[1],
+          d[2],
+        )
+        if coord_next in path: continue
+        char_at = rev_keypad.get((coord_next[0], coord_next[1]))
+        if char_at is None: continue
+        path_next = [
+          *path,
+          coord_next,
+        ]
+        if len(path_next) > max_moves: continue
+        if char_at == btn_to:
+          key_paths_cache[(btn_from, btn_to)].append(f'''{''.join(x[2] for x in path_next[1:])}A''')
+        else:
+          paths.appendleft(path_next)
+  if DEBUG > 1: print(f'{key_paths_cache=}')
+  for k, v in key_paths_cache.items():
+    assert len(v), f'key_paths_cache {k=} {v=}'
+  return key_paths_cache[(btn_from, btn_to)]
+
+
+def get_inputs(output: str, keypad: dict[str, tuple[int, int]]):
+  if DEBUG > 0: print(f'get_inputs {output=} {keypad=}')
+  assert isinstance(output, str), f'{output=} is not a str'
+  prefixed_output = f'A{output}'
   inputs: list[str] = []
-  prefixed_outputs = ['A', *outputs]
-  for btn_from, btn_to in zip(prefixed_outputs[:-1], prefixed_outputs[1:]):
-    coord_from = door_keypad[btn_from]
-    coord_to = door_keypad[btn_to]
-    move_row = coord_to[0] - coord_from[0]
-    move_col = coord_to[1] - coord_from[1]
-    row_moves = ['^' if move_row < 0 else 'v'] * abs(move_row)
-    col_moves = ['<' if move_col < 0 else '>'] * abs(move_col)
-    if btn_from in ['0', 'A'] or btn_to in ['0', 'A']:
-      # avoid gap
-      btn_inputs = [*col_moves, *row_moves, 'A']
+  for btn_from, btn_to in zip(prefixed_output[:-1], prefixed_output[1:]):
+    #build a set of all combinations from the cache
+    btn_paths = get_key_paths(btn_from, btn_to, keypad)
+    if len(inputs):
+      next_inputs: list[str] = []
+      for prev_input in inputs:
+        for btn_path in btn_paths:
+          next_inputs.append(f'{prev_input}{btn_path}')
+      inputs = next_inputs
     else:
-      btn_inputs = [*row_moves, *col_moves, 'A']
-    if DEBUG > 1:
-      print(f'door {btn_from=} {btn_to=} {coord_from=} {coord_to=} {move_row=} {move_col=} {btn_inputs=}')
-    inputs.extend(btn_inputs)
-  if DEBUG > 0: print(f'{outputs=} {inputs=}')
-  return inputs
-
-
-def gen_robot_inputs_robot(outputs: list[str], label: str) -> list[str]:
-  # will need to be called twice i think
-  inputs: list[str] = []
-  prefixed_outputs = ['A', *outputs]
-  for btn_from, btn_to in zip(prefixed_outputs[:-1], prefixed_outputs[1:]):
-    coord_from = robot_keypad[btn_from]
-    coord_to = robot_keypad[btn_to]
-    move_row = coord_to[0] - coord_from[0]
-    move_col = coord_to[1] - coord_from[1]
-    row_moves = ['^' if move_row < 0 else 'v'] * abs(move_row)
-    col_moves = ['<' if move_col < 0 else '>'] * abs(move_col)
-    # avoid gap
-    if btn_from in ['^', 'A'] or btn_to in ['^', 'A']:
-      btn_inputs = [*col_moves, *row_moves, 'A']
-    else:
-      btn_inputs = [*row_moves, *col_moves, 'A']
-    if DEBUG > 1:
-      print(f'{label} {btn_from=} {btn_to=} {coord_from=} {coord_to=} {move_row=} {move_col=} {btn_inputs=}')
-    inputs.extend(btn_inputs)
-  if DEBUG > 0: print(f'{outputs=} {inputs=}')
+      inputs = btn_paths
+  for x in inputs:
+    assert isinstance(x, str), f'{x=} is not a str'
+  if DEBUG > 0:
+    for x in inputs:
+      print(f'get_inputs input={x}')
   return inputs
 
 
 def part1():
   complexity_total = 0
   for door_code in door_codes:
-    door_keypresses = gen_robot_inputs_door(door_code)
-    robot_1_keypresses = gen_robot_inputs_robot(door_keypresses, 'robot1')
-    robot_2_keypresses = gen_robot_inputs_robot(robot_1_keypresses, 'robot2')
+    print(f'{door_code=}')
 
-    print(''.join(door_code))
-    print(''.join(door_keypresses))
-    print(''.join(robot_1_keypresses))
-    print(''.join(robot_2_keypresses))
-    len_robot_2_keypresses = len(robot_2_keypresses)
+    door_inputs = get_inputs(door_code, door_keypad)
+    # print(f'{door_inputs=}')
+
+    robot_1_inputs: list[str] = []
+    for x in door_inputs:
+      robot_1_inputs.extend(get_inputs(x, robot_keypad))
+    # print(f'{robot_1_inputs=}')
+
+    robot_2_inputs: list[str] = []
+    for x in robot_1_inputs:
+      robot_2_inputs.extend(get_inputs(x, robot_keypad))
+    # print(f'{robot_2_inputs=}')
+
+    shortest_robot_2_inputs = min(map(len, robot_2_inputs))
     door_code_int = int(''.join(x for x in door_code if x.isdigit()))
-    complexity = len_robot_2_keypresses * door_code_int
+    complexity = shortest_robot_2_inputs * door_code_int
     complexity_total += complexity
-    print(f'{len_robot_2_keypresses=} {door_code_int=} {complexity=}')
-    print()
-    # exit()
-  print(f'part 1: {complexity_total}')
-  #133696 too high
 
+    print(f'{shortest_robot_2_inputs=} {door_code_int=} {complexity=}')
+
+  print(f'part 1: {complexity_total}')
 
 part1()
