bdee43814409de1c4f9725617267e52c5e398e3a
adminadmin <git@akki.uk>
2024-12-22 11:27:09 +0000

    day 22 part 2: py fast

--- day22/solution.py
+++ day22/solution.py
@@ -47,11 +47,12 @@ def part1():
 
 
 def part2():
-  print('calc input_prices')
-  input_prices = []
+  sequence_totals: dict[tuple[int, int, int, int], int] = {}
   for value in inputs:
+    # build dict of first price per sequence (cache_key) for each input value
+    sequence_total: dict[tuple[int, int, int, int], int] = {}
     prices = []
-    for sequence_ix in range(2000):
+    for _ in range(2000):
       value ^= value << 6  # xor mult 64
       value &= 16777215  # mod 16777216
       value ^= value >> 5  # xor idiv 32
@@ -59,72 +60,24 @@ def part2():
       value ^= value << 11  # xor mult 2048
       value &= 16777215  # mod 16777216
       prices.append(value % 10)
-    input_prices.append(prices)
-  print(f'{input_prices[0]=}')
+    for i in range(1996):
+      cache_key = (
+        prices[i + 1] - prices[i],
+        prices[i + 2] - prices[i + 1],
+        prices[i + 3] - prices[i + 2],
+        prices[i + 4] - prices[i + 3],
+      )
+      if cache_key in sequence_total: continue
+      sequence_total[cache_key] = prices[i + 4]
+    # sum per-input value sequence prices into sequence_totals
+    for k, v in sequence_total.items():
+      sequence_totals[k] = sequence_totals.get(k, 0) + v
 
-  print('calc input_deltas')
-  input_deltas = [ \
-    [ \
-      price_next - price_prev \
-      for price_prev, price_next in zip(prices, prices[1:]) \
-    ] \
-    for prices in input_prices \
-  ]
-  print(f'{input_deltas[0]=}')
+  # then we can just sort it to get the best sequence and total
+  best_sequnce, best_total = sorted(sequence_totals.items(), key=lambda x: x[1], reverse=True)[0]
 
-  print('calc sequences')
-  # sequences = [[-2, 1, -1, 3]]
-  sequences = [
-    x for x in product(
-      range(-9, 10),
-      range(-9, 10),
-      range(-9, 10),
-      range(-9, 10),
-    ) if -9 <= sum(x[:2]) <= 9 and -9 <= sum(x[:3]) <= 9 and -9 <= sum(x) <= 9
-  ]
-  print(f'{len(sequences)=}')
-
-  print('calc sequence_totals')
-  best_total = 0
-  best_sequence = None
-  last_status = 0.0
-  for sequence_ix, sequence in enumerate(sequences):
-    sequence_total = 0
-    for input_delta_ix, deltas in enumerate(input_deltas):
-      delta_ix = -1
-      try:
-        while delta_ix := deltas.index(sequence[0], delta_ix + 1):
-          if deltas[delta_ix+1]==sequence[1] and \
-            deltas[delta_ix+2]==sequence[2] and \
-            deltas[delta_ix+3]==sequence[3]:
-            sequence_total += input_prices[input_delta_ix][delta_ix + 4]
-            break
-      except:
-        pass
-      # for d0, d1, d2, d3, p in zip(
-      #   deltas,
-      #   deltas[1:],
-      #   deltas[2:],
-      #   deltas[3:],
-      #   input_prices[input_delta_ix][4:],
-      # ):
-      #   if d0==sequence[0] and \
-      #     d1==sequence[1] and \
-      #     d2==sequence[2] and \
-      #     d3==sequence[3]:
-      #     # print(f'{d0=} {d1=} {d2=} {d3=} {p=} {j=}')
-      #     sequence_total += p
-      #     break
-    if time.monotonic() >= last_status + 10:
-      last_status = time.monotonic()
-      print(f'{last_status%3600:04.0f} [{sequence_ix}/{len(sequences)}] {sequence=} {best_sequence=} {best_total=}')
-    if sequence_total > best_total:
-      best_total = sequence_total
-      best_sequence = sequence
-      print(f'{best_sequence=} {best_total=}')
-
-  print(f'part 2: {best_sequence=} {best_total=}')
+  print(f'part 2: {best_sequnce=} {best_total=}')
 
 
-# part1()
+part1()
 part2()
