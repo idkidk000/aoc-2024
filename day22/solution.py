42d21551f8db1c3b979daf7c7d7c1f3cbb8684d1
adminadmin <git@akki.uk>
2024-12-22 09:41:01 +0000

    day 22 part 2: py but it's very slow

--- day22/solution.py
+++ day22/solution.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
-from functools import cache
+# from functools import cache
+from itertools import product
 
 sys.setrecursionlimit(1_000_000)
 DEBUG = 0
@@ -44,4 +45,54 @@ def part1():
   print(f'part 1: {total=}')
 
 
-part1()
+def part2():
+  print('calc input_deltas')
+  input_deltas = []
+  for value_prev in inputs:
+    deltas = []
+    for i in range(2000):
+      value = value_prev
+      value ^= value << 6  # xor mult 64
+      value &= 16777215  # mod 16777216
+      value ^= value >> 5  # xor idiv 32
+      value &= 16777215  # mod 16777216
+      value ^= value << 11  # xor mult 2048
+      value &= 16777215  # mod 16777216
+      delta = (value % 10) - (value_prev % 10)
+      deltas.append(delta)
+    input_deltas.append(deltas)
+  for x in input_deltas:
+    for y in x:
+      assert -9 <= y <= 9, f'delta {y=} invalid'
+
+  print('calc sequences')
+  sequences = [
+    x for x in product(
+      range(-9, 10),
+      range(-9, 10),
+      range(-9, 10),
+      range(-9, 10),
+    ) if -9 <= x[0] + x[1] <= 9 and -9 <= x[0] + x[1] + x[2] <= 9 and -9 <= sum(x) <= 9
+  ]
+  print(f'{len(sequences)=}')
+
+  print('calc sequence_totals')
+  sequence_totals = []
+  for j, sequence in enumerate(sequences):
+    if j % 50 == 0: print(f'[{j}/{len(sequences)}] {sequence}')
+    sequence_total = 0
+    for deltas in input_deltas:
+      for i in range(1996):
+        if deltas[i] == sequence[0] and deltas[i + 1] == sequence[1] and deltas[i + 2] == sequence[2] and deltas[
+          i + 3] == sequence[3]:
+          sequence_total += deltas[i + 4]
+          break
+    sequence_totals.append((sequence, sequence_total))
+
+  print('sort sequence_totals')
+  best_sequence = sorted(sequence_totals, key=lambda x: x[1], reverse=True)[0]
+  print(f'part 2: {best_sequence=}')
+
+
+# part1()
+part2()
