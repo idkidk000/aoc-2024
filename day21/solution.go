e81cd1bf613e4f31f3e165c4ca3f53899dcd0bb8
adminadmin <git@akki.uk>
2024-12-23 21:03:30 +0000

    day 21 part 1: go and it's a a lot faster (but we'll still need a lengths cache for part 2)

--- day21/solution.go
+++ day21/solution.go
@@ -44,88 +44,91 @@ func main() {
 	if debug > 0 {
 		fmt.Println("codes", codes)
 	}
-	numericMoves := walkKeypad(numericKeypad,0)
+	numericMoves := walkKeypad(numericKeypad, 0)
 	if debug > 0 {
 		fmt.Println(numericMoves)
 	}
-	directionalMoves := walkKeypad(directionalKeypad,0)
+	directionalMoves := walkKeypad(directionalKeypad, 0)
 	if debug > 0 {
 		fmt.Println(directionalMoves)
 	}
-	part1(codes,numericMoves,directionalMoves,debug)
+	part1(codes, numericMoves, directionalMoves, debug)
 }
 
-func part1(codes []string, numericMoves map[string][]string,directionalMoves map[string][]string,debug uint8){
-	totalComplexity:=0
-	for _,code:=range codes{
-		sequences:=transcribe(code,numericMoves,debug)
+func part1(codes []string, numericMoves map[string][]string, directionalMoves map[string][]string, debug uint8) {
+	totalComplexity := 0
+	for _, code := range codes {
+		sequences := transcribe(code, numericMoves, debug)
 		// fmt.Println("code",code,"sequences",sequences)
-		for depth:=0;depth<2;depth++{
-			nextSequences:=[]string{}
-			for _,sequence:=range sequences{
-				for _,nextSequence:=range transcribe(sequence,directionalMoves,debug){
+		for depth := 0; depth < 2; depth++ {
+			// overallocating might be a bit faster than multiple allocations
+			nextSequences := make([]string, 0, len(sequences))
+			for _, sequence := range sequences {
+				for _, nextSequence := range transcribe(sequence, directionalMoves, debug) {
 					nextSequences = append(nextSequences, nextSequence)
 				}
 			}
 			sort.Slice(nextSequences, func(i, j int) bool {
-				return len(nextSequences[i])<len(nextSequences[j])
+				return len(nextSequences[i]) < len(nextSequences[j])
 			})
-			shortest:=len(nextSequences[0])
-			sequences=[]string{}
-			for _,sequence:=range nextSequences{
-				if len(sequence)==shortest{
+			shortest := len(nextSequences[0])
+			// overallocating might be a bit faster than multiple allocations
+			sequences = make([]string, 0, len(nextSequences))
+			for _, sequence := range nextSequences {
+				if len(sequence) == shortest {
 					sequences = append(sequences, sequence)
 				}
 			}
 		}
 		// fmt.Println("code",code,"sequences",sequences)
-		intCode,_:=strconv.Atoi(code[0:len(code)-1]);
-		sequenceLen:=len(sequences[0])
-		complexity:=sequenceLen*intCode
-		totalComplexity+=complexity
-		fmt.Println("code",code,"intCode",intCode,"sequenceLen",sequenceLen,"complexity",complexity)
+		intCode, _ := strconv.Atoi(code[0 : len(code)-1])
+		sequenceLen := len(sequences[0])
+		complexity := sequenceLen * intCode
+		totalComplexity += complexity
+		fmt.Println("code", code, "intCode", intCode, "sequenceLen", sequenceLen, "complexity", complexity)
 	}
-	fmt.Println("part 1:",totalComplexity)
+	fmt.Println("part 1:", totalComplexity)
 }
 
-func transcribe(code string,moves map[string][]string, debug uint8)([]string){
+func transcribe(code string, moves map[string][]string, debug uint8) []string {
 	// accepts either the numeric or direction keypad moves and transcribes to another layer of directional moves
-	sequences:=make([][]string,0,len(code))
-	prefixedCode:=fmt.Sprintf("A%s",code)
-	for i:=1;i<len(prefixedCode);i++{
-		index:=prefixedCode[i-1:i+1]
-		subsequence:=moves[index]
+	sequences := make([][]string, 0, len(code))
+	prefixedCode := fmt.Sprintf("A%s", code)
+	for i := 1; i < len(prefixedCode); i++ {
+		index := prefixedCode[i-1 : i+1]
+		subsequence := moves[index]
 		sequences = append(sequences, subsequence)
-		if debug>1 {
-			fmt.Println("transcribe code",code,"i",i,"index",index,"seqs",subsequence)
+		if debug > 1 {
+			fmt.Println("transcribe code", code, "i", i, "index", index, "seqs", subsequence)
 		}
 	}
 	//for the naive part 1 approach, this returns a []string of the shortest products of each option
-	if debug>1 {
-		fmt.Println("sequences:",sequences)
+	if debug > 1 {
+		fmt.Println("sequences:", sequences)
 	}
-	products:=make([]string,len(sequences[0]))
-	copy(products,sequences[0])
-	if debug>1 {
-		fmt.Println("products:",products)
+	products := make([]string, len(sequences[0]))
+	copy(products, sequences[0])
+	if debug > 1 {
+		fmt.Println("products:", products)
 	}
-	for _,group:=range sequences[1:]{
-		if debug>1 {
-			fmt.Println("group:",group)
+	for _, group := range sequences[1:] {
+		if debug > 1 {
+			fmt.Println("group:", group)
 		}
-		nextProducts:=[]string{}
-		for _,option:=range group{
-			for _,product:=range products{
-				nextProducts = append(nextProducts, fmt.Sprintf("%s%s",product,option))
+		nextProducts := make([]string, 0, len(group)*len(products))
+		for _, option := range group {
+			for _, product := range products {
+				nextProducts = append(nextProducts, product+option)
 			}
 		}
 		sort.Slice(nextProducts, func(i, j int) bool {
-			return len(nextProducts[i])<len(nextProducts[j])
+			return len(nextProducts[i]) < len(nextProducts[j])
 		})
-		shortest:=len(nextProducts[0])
-		products=[]string{}
-		for _,nextProduct:=range nextProducts{
-			if len(nextProduct)==shortest {
+		shortest := len(nextProducts[0])
+		//overallocating
+		products = make([]string, 0, len(nextProducts))
+		for _, nextProduct := range nextProducts {
+			if len(nextProduct) == shortest {
 				products = append(products, nextProduct)
 			}
 		}
@@ -146,14 +149,14 @@ func walkKeypad(keypad [][]string, debug uint8) (moves map[string][]string) {
 			}
 		}
 	}
-	if debug>1 {
-		fmt.Println("keyMap",keyMap)
+	if debug > 1 {
+		fmt.Println("keyMap", keyMap)
 	}
 	// walk from each button to each button. add each valid path to moves
 	for fromKey, fromCoord := range keyMap {
 		for toKey, toCoord := range keyMap {
-			if debug>1 {
-				fmt.Println("fromKey",fromKey,"toKey",toKey)
+			if debug > 1 {
+				fmt.Println("fromKey", fromKey, "toKey", toKey)
 			}
 			minMoves := rows * cols
 			// cache key for moves map
@@ -168,7 +171,7 @@ func walkKeypad(keypad [][]string, debug uint8) (moves map[string][]string) {
 			// init paths
 			paths := [][]CoordPress{{CoordPress{fromCoord.r, fromCoord.c, ""}}}
 			for len(paths) > 0 {
-				if debug>1 {
+				if debug > 1 {
 					fmt.Println("len paths loop top")
 				}
 				// pop left
@@ -178,8 +181,8 @@ func walkKeypad(keypad [][]string, debug uint8) (moves map[string][]string) {
 					continue
 				}
 				current := path[len(path)-1]
-				if debug>1 {
-					fmt.Println("path",path,"len(paths)",len(paths),"current",current)
+				if debug > 1 {
+					fmt.Println("path", path, "len(paths)", len(paths), "current", current)
 				}
 				for d := 0; d < 4; d++ {
 					next := CoordPress{current.r, current.c, ""}
@@ -199,15 +202,15 @@ func walkKeypad(keypad [][]string, debug uint8) (moves map[string][]string) {
 					}
 					// oob
 					if next.r < 0 || next.r >= rows || next.c < 0 || next.c >= cols {
-						if debug>1 {
-							fmt.Println("oob",next)
+						if debug > 1 {
+							fmt.Println("oob", next)
 						}
 						continue
 					}
 					// missing key
-					if keypad[next.r][next.c]=="" {
-						if debug>1{
-							fmt.Println("missing key",next)
+					if keypad[next.r][next.c] == "" {
+						if debug > 1 {
+							fmt.Println("missing key", next)
 						}
 						continue
 					}
@@ -220,14 +223,14 @@ func walkKeypad(keypad [][]string, debug uint8) (moves map[string][]string) {
 						}
 					}
 					if duplicate {
-						if debug>1 {
+						if debug > 1 {
 							fmt.Println("duplicate", next)
 						}
 						continue
 					}
 					if next.r == toCoord.r && next.c == toCoord.c {
-						if debug>1 {
-							fmt.Println("reached end",path,next)
+						if debug > 1 {
+							fmt.Println("reached end", path, next)
 						}
 						// push the keys and "A" to moves[movesIndex]=[]
 						presses := make([]string, len(path)+1)
@@ -236,31 +239,31 @@ func walkKeypad(keypad [][]string, debug uint8) (moves map[string][]string) {
 						}
 						presses[len(path)-1] = next.b
 						presses[len(path)] = "A"
-						if debug>1 {
-							fmt.Println(movesIndex,"found path",presses,path)
+						if debug > 1 {
+							fmt.Println(movesIndex, "found path", presses, path)
 						}
 						moves[movesIndex] = append(moves[movesIndex], strings.Join(presses, ""))
 						minMoves = len(path)
-						if (debug>1 ){
-							fmt.Println("reached end done",path,next)
+						if debug > 1 {
+							fmt.Println("reached end done", path, next)
 						}
 						break
 					} else {
-						if debug>1 {
+						if debug > 1 {
 							fmt.Println("continue", next)
 						}
 						nextPath := make([]CoordPress, len(path)+1)
 						copy(nextPath, path)
 						nextPath[len(path)] = next
 						paths = append(paths, nextPath)
-						if debug>1 {
-							fmt.Println("continue done. path:",path,"next:",next,"nextPath",nextPath)
+						if debug > 1 {
+							fmt.Println("continue done. path:", path, "next:", next, "nextPath", nextPath)
 						}
 					}
 				}
 			}
-			if debug>1{
-				fmt.Println("moves=",moves)
+			if debug > 1 {
+				fmt.Println("moves=", moves)
 			}
 		}
 	}
