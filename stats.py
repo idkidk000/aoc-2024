1c65e6ca4a7fdd052c60d869ee4d5870a217c994
adminadmin <git@akki.uk>
2024-12-23 14:49:34 +0000

    stats clean up file detection

--- stats.py
+++ stats.py
@@ -8,12 +8,12 @@ GAP='  '
 
 files = sorted(os.walk('.'))
 all_exts = sorted({
-  z \
+  z[-1] \
   for x in files if re.match(DIR_REGEX, x[0]) \
-  for y in x[2] if not y.startswith('.') and '.' in y and (z:=y.split('.')[1]) and z not in IGNORE_EXTS
+  for y in x[2] if not y.startswith('.') and len(z:=y.split('.'))>1 and z[-1] not in IGNORE_EXTS
 })
 for dir_path, dir_names, file_names in files:
   if len(file_names) == 0 or not re.match(DIR_REGEX, dir_path): continue
   day = dir_path.removeprefix('./')
-  exts = [x.split('.')[-1] for x in file_names]
+  exts = [x.split('.')[-1] for x in file_names if not x.startswith('.')]
   print(f'''{day}{GAP}{GAP.join([x if x in exts else (' ' * len(x)) for x in all_exts])}''')
