d573241f315130039ce7a0804dd9e2a3a8a0c3ba
adminadmin <git@akki.uk>
2024-12-26 17:33:06 +0000

    day05: c++ refactor a bit

--- day05/solution.cpp
+++ day05/solution.cpp
@@ -20,6 +20,16 @@ struct Args {
   bool part2 = true;
 };
 
+struct IteratorAndIndex {
+  std::vector<int>::iterator iterator;
+  int index;
+};
+
+struct OrderingRule {
+  int before;
+  int after;
+};
+
 Args parseArgs(int argc, char *argv[]) {
   Args args;
   std::unordered_map<std::string, std::function<void()>> argMap = {
@@ -55,9 +65,9 @@ Args parseArgs(int argc, char *argv[]) {
   return args;
 }
 
-std::pair<std::vector<std::pair<int, int>>, std::vector<std::vector<int>>>
+std::pair<std::vector<OrderingRule>, std::vector<std::vector<int>>>
 readData(std::string filename, int debug) {
-  std::vector<std::pair<int, int>> rules;
+  std::vector<OrderingRule> rules;
   std::vector<std::vector<int>> updates;
   std::ifstream file(filename);
   int section = 0;
@@ -71,12 +81,12 @@ readData(std::string filename, int debug) {
       switch (section) {
       case 0: {
         auto split = line.find("|");
-        auto left = std::stoi(line.substr(0, split));
-        auto right = std::stoi(line.substr(split + 1));
-        rules.push_back({left, right});
+        auto before = std::stoi(line.substr(0, split));
+        auto after = std::stoi(line.substr(split + 1));
+        rules.push_back({before, after});
         if (debug > 1) {
           std::cout << "rule: " << line << " split: " << split
-                    << " left: " << left << " right: " << right << "\n";
+                    << " before: " << before << " after: " << after << "\n";
         }
         break;
       }
@@ -108,27 +118,26 @@ readData(std::string filename, int debug) {
   return {rules, updates};
 }
 
-void part1(std::vector<std::pair<int, int>> rules,
-           std::vector<std::vector<int>> updates, int debug) {
+IteratorAndIndex indexOf(std::vector<int> &vec, int val) {
+  auto it = std::find(vec.begin(), vec.end(), val);
+  if (it == vec.end())
+    return {it, -1};
+  int ix = std::distance(vec.begin(), it);
+  return {it, ix};
+}
+
+void part1(std::vector<OrderingRule> &rules,
+           std::vector<std::vector<int>> &updates, int debug) {
   int sumMiddle = 0;
-  for (const auto &update : updates) {
+  for (auto &update : updates) {
     bool ok = true;
     for (const auto &rule : rules) {
-      auto leftIt = std::find(update.begin(), update.end(), rule.first);
-      auto rightIt = std::find(update.begin(), update.end(), rule.second);
-      if (leftIt == update.end() || rightIt == update.end()) {
+      auto beforeIx = indexOf(update, rule.before).index;
+      auto afterIx = indexOf(update, rule.after).index;
+      if (!(beforeIx == -1 || afterIx == -1 || beforeIx < afterIx)) {
         if (debug > 1)
-          std::cout << "rule not matched\n";
-        continue;
-      }
-      auto leftIx = std::distance(update.begin(), leftIt);
-      auto rightIx = std::distance(update.begin(), rightIt);
-      auto correctOrder = leftIx < rightIx;
-      if (debug > 1)
-        std::cout << "rule matched leftIx=" << leftIx << " rightIx=" << rightIx
-                  << " correct order: " << std::boolalpha << correctOrder
-                  << "\n";
-      if (!correctOrder) {
+          std::cout << "incorrect order beforeIx=" << beforeIx
+                    << " afterIx=" << afterIx << "\n";
         ok = false;
         break;
       }
@@ -139,70 +148,88 @@ void part1(std::vector<std::pair<int, int>> rules,
   std::cout << "part 1: " << sumMiddle << "\n";
 }
 
-void part2(std::vector<std::pair<int, int>> rules,
-           std::vector<std::vector<int>> updates, int debug) {
-  // FIXME: this is horrible
+void part2(std::vector<OrderingRule> &rules,
+           std::vector<std::vector<int>> &updates, int debug) {
   int sumMiddle = 0;
   for (auto &update : updates) {
     bool ok = true;
+    std::vector<OrderingRule> applicableRules;
     for (const auto &rule : rules) {
-      auto leftIt = std::find(update.begin(), update.end(), rule.first);
-      auto rightIt = std::find(update.begin(), update.end(), rule.second);
-      if (leftIt == update.end() || rightIt == update.end()) {
-        if (debug > 1)
-          std::cout << "rule not matched\n";
-        continue;
-      }
-      auto leftIx = std::distance(update.begin(), leftIt);
-      auto rightIx = std::distance(update.begin(), rightIt);
-      auto correctOrder = leftIx < rightIx;
-      if (debug > 1)
-        std::cout << "rule matched leftIx=" << leftIx << " rightIx=" << rightIx
-                  << " correct order: " << std::boolalpha << correctOrder
+      auto beforeIx = indexOf(update, rule.before).index;
+      auto afterIx = indexOf(update, rule.after).index;
+      auto matched = beforeIx != -1 && afterIx != -1;
+      auto correctOrder = beforeIx == -1 || afterIx == -1 || beforeIx < afterIx;
+      if (matched && debug > 1) {
+        std::cout << "rule matched: " << std::boolalpha << matched
+                  << " correctOrder: " << std::boolalpha << correctOrder
+                  << " beforeIx: " << beforeIx << " afterIx: " << afterIx
                   << "\n";
+      }
+      if (matched)
+        applicableRules.push_back(rule);
       if (!correctOrder) {
         ok = false;
-        break;
+        // DO NOT BREAK OUT EARLY - need to finish building applicableRules
       }
     }
     if (!ok) {
-      bool moved = true;
-      while (moved) {
-        moved = false;
-        // TODO: loop over rules, move elems in update, set moved to true
-        for (const auto rule : rules) {
-          auto leftIt = std::find(update.begin(), update.end(), rule.first);
-          auto rightIt = std::find(update.begin(), update.end(), rule.second);
-          if (leftIt == update.end() || rightIt == update.end()) {
-            if (debug > 1)
-              std::cout << "rule not matched\n";
-            continue;
-          }
-          auto leftIx = std::distance(update.begin(), leftIt);
-          auto rightIx = std::distance(update.begin(), rightIt);
-
-          auto correctOrder = leftIx < rightIx;
-          if (debug > 1)
-            std::cout << "rule matched leftIx=" << leftIx
-                      << " rightIx=" << rightIx
-                      << " correct order: " << std::boolalpha << correctOrder
-                      << "\n";
-
-          if (correctOrder)
-            continue;
-          if (debug > 0)
-            std::cout << "rule: " << rule.first << " before: " << rule.second
-                      << "; move " << rule.first << " from ix: " << leftIx
-                      << " to before ix: " << rightIx;
-          update.erase(leftIt);
-          update.insert(rightIt, rule.first);
-          moved = true;
+      if (debug > 0) {
+        std::cout << "applicable rules:\n";
+        for (const auto &rule : rules) {
+          std::cout << "before: " << rule.before << " after: " << rule.after
+                    << "\n";
         }
+      }
+      bool moved = true;
+      int loopCount = 0;
+      while (moved) {
+        if (debug > 1)
+          std::cout << "loopCount" << loopCount << "\n";
+        moved = false;
+        // loop over only the applicable subset of rules to be a bit less
+        // horrible
+        // there's almost certainly a better way of doing this
+        for (const auto &rule : applicableRules) {
+          auto before = indexOf(update, rule.before);
+          auto after = indexOf(update, rule.after);
+          // don't need -1 check
+          if (debug > 0)
+            std::cout << "rule: before: " << rule.before
+                      << " after: " << rule.after
+                      << " correct: " << std::boolalpha
+                      << (before.index < after.index) << "\n";
+          if (before.index < after.index)
+            continue;
+          if (debug > 0) {
+            std::cout << "before move: ";
+            for (const auto i : update) {
+              std::cout << " " << i;
+            }
+            std::cout << "\n";
+            std::cout << "move from ix: " << before.index
+                      << " to before ix: " << after.index << "\n";
+          }
+
+          assert(before.index > -1 && after.index > -1);
+
+          // delete first, since before is incorrectly to the right of after
+          update.erase(before.iterator);
+          update.insert(after.iterator, rule.before);
+          moved = true;
+          if (debug > 0) {
+            std::cout << "after move: ";
+            for (const auto i : update) {
+              std::cout << " " << i;
+            }
+            std::cout << "\n";
+          }
+        }
+        ++loopCount;
       }
       sumMiddle += update[update.size() / 2];
     }
   }
-  std::cout << "part 1: " << sumMiddle << "\n";
+  std::cout << "part 2: " << sumMiddle << "\n";
 }
 
 int main(int argc, char *argv[]) {
