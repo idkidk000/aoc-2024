c27f4d2b15fbbb543d31da5ed4c7a65c62f4f89c
adminadmin <git@akki.uk>
2025-01-02 20:26:51 +0000

    day21: c++ quick debloat (still incorrect) (still massive)

--- day21/solution.cpp
+++ day21/solution.cpp
@@ -1,7 +1,5 @@
 #include <cassert>
 #include <climits>
-#include <concepts>
-#include <cstddef>
 #include <cstdlib>
 #include <deque>
 #include <fstream>
@@ -9,7 +7,6 @@
 #include <iostream>
 #include <map>
 #include <ostream>
-#include <set>
 #include <stdexcept>
 #include <string>
 #include <sys/types.h>
@@ -20,42 +17,19 @@ struct Args {
   std::string filename = "input.txt";
   int debug = 0;
   bool part1 = true, part2 = true;
+  operator std::string() const {
+    return std::format("{}; debug: {}; part1: {}; part2: {}", filename, debug, part1, part2);
+  }
 };
 
 struct RowCol {
   int r, c;
   bool operator==(const RowCol &rhs) const { return r == rhs.r && c == rhs.c; }
   bool operator<(const RowCol &rhs) const { return r != rhs.r ? r < rhs.r : c < rhs.c; }
-  //return a new instance
   RowCol operator+(const RowCol &rhs) const { return RowCol{r + rhs.r, c + rhs.c}; }
   RowCol operator-(const RowCol &rhs) const { return RowCol{r - rhs.r, c - rhs.c}; }
 };
 
-struct RowColHash {
-  std::size_t operator()(const RowCol &rhs) const { return std::hash<int>()(rhs.r) ^ (std::hash<int>()(rhs.c) << 1); }
-};
-
-struct TextGrid {
-  std::string data = "";
-  int rows = 0, cols = 0;
-
-  const std::array<RowCol, 8> d8 = {{{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}}};
-  char at(int row, int col) { return oob(row, col) ? ' ' : data.at(row * cols + col); }
-  std::vector<RowCol> findAll(char c) {
-    std::vector<RowCol> result;
-    for (int i = data.find(c); i != std::string::npos; i = data.find(c, i + 1)) result.push_back({i / cols, i % cols});
-    return result;
-  }
-  RowCol findFirst(char c) { return toRowCol(data.find(c)); }
-  void load(std::string line) { data += line, cols = line.size(), rows = data.size() / cols; }
-  bool oob(int row, int col) { return row < 0 || row >= rows || col < 0 || col >= cols; };
-  void put(int row, int col, char c) { data.replace(row * cols + col, 1, std::string(1, c)); }
-  size_t size() { return data.size(); }
-  RowCol toRowCol(int ix) { return {ix / cols, ix % cols}; }
-  std::set<char> unique() { return {data.begin(), data.end()}; };
-};
-
-//still waiting to find out that these very normal functions do exist in std but have silly names
 template <typename T> bool in(T val, std::vector<T> vec) { return std::find(vec.begin(), vec.end(), val) != vec.end(); }
 template <typename T> std::string join(std::vector<T> vec, std::string sep = ",") {
   std::string result = "";
@@ -67,41 +41,6 @@ template <typename T> std::string join(std::vector<T> vec, std::string sep = ","
   }
   return result.size() > sep.size() ? result.substr(0, result.size() - sep.size()) : result;
 }
-std::vector<std::string> split(std::string val, std::string sep = " ") {
-  std::vector<std::string> result = {};
-  int li = 0; //stringstream and getline only work if sep is a char
-  for (size_t i = val.find(sep); i != std::string::npos; i = val.find(sep, li))
-    result.push_back(val.substr(li, i)), li = i + sep.size();
-  result.push_back(val.substr(li));
-  return result;
-}
-template <std::integral T> T pMod(T val, T mod) { //any integer type
-  const auto result = val % mod;
-  return result < 0 ? result + mod : result;
-}
-std::string replaceAll(std::string val, std::string find, std::string replace) {
-  std::string result = "";
-  int li = 0;
-  for (size_t i = val.find(find); i != std::string::npos; i = val.find(find, li))
-    result += val.substr(li, i), li = i + find.size();
-  result += val.substr(li);
-  return result;
-}
-
-//std::format
-template <> struct std::formatter<RowCol> : std::formatter<std::string> {
-  auto format(const RowCol &v, auto &ctx) {
-    return std::formatter<std::string>::format("{" + std::to_string(v.r) + "," + std::to_string(v.c) + "}", ctx);
-  };
-};
-
-//std::cout
-std::ostream &operator<<(std::ostream &os, const Args &v) {
-  return os << v.filename << "; debug=" << v.debug << "; part 1: " << (v.part1 ? "true" : "false")
-            << "; part 2: " << (v.part2 ? "true" : "false");
-}
-std::ostream &operator<<(std::ostream &os, const RowCol &v) { return os << "{" << v.r << "," << v.c << "}"; }
-std::ostream &operator<<(std::ostream &os, const TextGrid &v) { return os << "{" << v.rows << "x" << v.cols << "}"; }
 
 Args parseArgs(int argc, char *argv[]) {
   Args args;
@@ -116,7 +55,7 @@ Args parseArgs(int argc, char *argv[]) {
     else
       throw std::runtime_error("unknown arg: " + arg);
   }
-  std::cout << args << "\n";
+  std::cout << (std::string)args << "\n";
   return args;
 }
 #pragma endregion
