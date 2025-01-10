21f76ca7c1158dcbd500eb833c05d301943efa60
adminadmin <git@akki.uk>
2025-01-10 12:45:04 +0000

    stats: accept dirname search arg

--- stats.py
+++ stats.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import os
 import re
+import sys
 
 IGNORE_EXTS = ['txt']
 DIR_REGEX = re.compile(r'^\./20[\d]{2}/day\d+$')
-GAP='  '
+GAP = '  '
+SEARCH = sys.argv[1] if len(sys.argv) > 1 else ''
 
 files = sorted(os.walk('.'))
 all_exts = sorted({
@@ -14,6 +16,7 @@ all_exts = sorted({
 })
 for dir_path, dir_names, file_names in files:
   if len(file_names) == 0 or not re.match(DIR_REGEX, dir_path): continue
+  if SEARCH not in dir_path: continue
   day = dir_path.removeprefix('./')
   exts = [x.split('.')[-1] for x in file_names if x.startswith('solution.')]
   print(f'''{day}{GAP}{GAP.join([x if x in exts else (' ' * len(x)) for x in all_exts])}''')
