22745ff40488bbf2ba3ca3425749f00ce213d9f3
adminadmin <git@akki.uk>
2025-01-10 17:40:13 +0000

    2023 day 17 part 1: python and its fast

--- 2023/day17/solution.py
+++ 2023/day17/solution.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import sys
-from collections import deque
+# from collections import deque
+from typing import Any
+from queue import PriorityQueue
 
 sys.setrecursionlimit(1_000_000)
 DEBUG = 0
@@ -27,15 +29,15 @@ for arg in sys.argv[1:]:
 with open(FILENAME, 'r') as f:
   text = f.read()
 # if DEBUG: print(f'{text=}')
-grid = [list(x) for x in text.splitlines()]
+grid = [[int(y) for y in x] for x in text.splitlines()]
 rows = len(grid)
 cols = len(grid[0])
 
 
-def draw_grid(grid: list[list[str]]):
+def draw_grid(grid: list[list[Any]]):
   if DEBUG:
     for r in range(rows):
-      print(f'''{r: 3d}  {''.join(grid[r])}''')
+      print(f'''{r: 3d}  {''.join(str(x) for x in grid[r])}''')
     print(f'{rows=} {cols=}')
 
 
@@ -51,14 +53,14 @@ class Path():
   def __init__(self, r: int, c: int, d: int, cost: int = 0, turned: int = 0, hist: list[tuple[int, int]] = []):
     self.r, self.c, self.d = r, c, d
     self.cost, self.turned = cost, turned
-    self.hist = [*hist, (r, c)]
+    self.hist = []  #[*hist, (r, c)]
 
   def move(self, turn: int):
     if turn == 0 and self.turned >= 2: return None
     d = (self.d + turn) % 4
     r, c = self.r + D4[d][0], self.c + D4[d][1]
     if r < 0 or r >= rows or c < 0 or c >= cols: return None
-    cost = self.cost + int(grid[r][c])
+    cost = self.cost + grid[r][c]
     if turn == 0: turned = self.turned + 1
     else: turned = 0
     return Path(r, c, d, cost, turned, self.hist)
@@ -71,34 +73,64 @@ class Path():
   def rcd(self):
     return (self.r, self.c, self.d)
 
+  @property
+  def rcdt(self):
+    return (self.r, self.c, self.d, self.turned)
+
+  @property
+  def rct(self):
+    return (self.r, self.c, self.turned)
+
+  def __lt__(self, other) -> bool:
+    return self.cost < other.cost
+
+  def __repr__(self) -> str:
+    return f'<Path r={self.r} c={self.c} d={self.d} cost={self.cost} turned={self.turned}>'
+
 
 def part1():
   start = (0, 0, 1)
   end = (rows - 1, cols - 1)
-  # need to account for direction, which slows things down considerably
-  tile_costs: dict[tuple[int, int, int], int] = {start: 0}
-  paths = deque([Path(*start)])
+  #rcdt
+  tile_costs: dict[tuple[int, int, int, int], int] = {(0, 0, 1, 0): 0}
+  paths: PriorityQueue[Path] = PriorityQueue()
+  paths.put(Path(*start))
   best_path = None
-  while len(paths):
-    path = paths.pop()
-    if path.cost > tile_costs[path.rcd]: continue
+  best_cost = None
+  prev_cost = None
+  while paths.qsize() > 0:
+    path = paths.get()
+    if best_cost and best_cost < path.cost: continue
+    if path.cost > tile_costs[path.rcdt]: continue
+    if DEBUG > 1 and path.cost != prev_cost:
+      print(f'{path=} {paths.qsize()=}')
+      prev_cost = path.cost
     for t in [-1, 0, 1]:
       if not (next_path := path.move(t)): continue
-      if next_path.rcd in tile_costs and tile_costs[next_path.rcd] < next_path.cost: continue
-      tile_costs[next_path.rcd] = next_path.cost
+      if best_cost and best_cost < next_path.cost: continue
+      if next_path.rcdt in tile_costs and tile_costs[next_path.rcdt] <= next_path.cost: continue
+      tile_costs[next_path.rcdt] = next_path.cost
       if next_path.rc == end:
         if best_path is None or best_path.cost > next_path.cost:
           best_path = next_path
+          best_cost = next_path.cost
+          if DEBUG > 1: print(f'{best_path.cost=} {paths.qsize()=:,}')
         continue
-      paths.append(next_path)
+      # push more expensive paths to the front of the queue so we can start pruning sooner
+      # if tile_costs[next_path.rc] < next_path.cost: paths.appendleft(next_path)
+      # else: paths.append(next_path)
+      # if tile_costs[next_path.rc] < next_path.cost: paths.append(next_path)
+      # else: paths.appendleft(next_path)
+      #trying a priorityqueue
+      paths.put(next_path)
   assert best_path
-  if DEBUG > 0:
-    grid_copy = [[y for y in x] for x in grid]
-    for r, c in best_path.hist:
-      item = grid_copy[r][c]
-      grid_copy[r][c] = f'\x1b[1;31m{item}\x1b[0m'
-    draw_grid(grid_copy)
-  print(f'part 1: {best_path.cost}')
+  # if DEBUG > 0:
+  #   grid_copy = [[y for y in x] for x in grid]
+  #   for r, c in best_path.hist:
+  #     item = grid_copy[r][c]
+  #     grid_copy[r][c] = f'\x1b[1;31m{item}\x1b[0m'
+  #   draw_grid(grid_copy)
+  # print(f'part 1: {best_path.cost}')
 
 
 def part2():
