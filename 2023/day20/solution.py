cee184f00649db306b5dd61815b4188cc0468cea
adminadmin <git@akki.uk>
2025-01-12 15:04:05 +0000

    day 20 pt 2: not working yet but i have an idea

--- 2023/day20/solution.py
+++ 2023/day20/solution.py
@@ -2,6 +2,7 @@
 import sys
 import re
 import json
+from collections import defaultdict
 
 sys.setrecursionlimit(1_000_000)
 DEBUG = 0
@@ -38,7 +39,7 @@ if DEBUG > 2:
   else: print(f'{modules=}')
 
 
-def part1():
+def solve(iterations=1000, rx_trap=False):
   # initial states
   flip_flops = {k: LOW for k, v in modules.items() if v['type'] == FLIP_FLOP}
   conjunctions = {
@@ -58,9 +59,20 @@ def part1():
     else:
       print(f'{flip_flops=}')
       print(f'{conjunctions=}')
+  # # yapf: disable
+  # rx_conjunction = [k for k, v in modules.items() if v['type'] == CONJUNCTION and 'rx' in v['targets']][0]  #type: ignore
+  # # lol these are also conjunctions. may need to go a level deeper
+  # # yep none of them went high after 100k cycles
+  # # rx_conjuncion_drivers: dict[str, dict[int, bool]] = {k: {} for k, v in modules.items() if rx_conjunction in v['targets']} #type: ignore
+  # rx_l1=[k for k,v in modules.items() if rx_conjunction in v['targets']] #type: ignore
+  # rx_l2:dict[str,list[tuple[int,bool]]]={k:[] for k,v in modules.items() if any(x in v['targets'] for x in rx_l1)} #type: ignore
+  # rx_l3:dict[str,list[tuple[int,bool]]]={k:[] for k,v in modules.items() if any(x in v['targets'] for x in rx_l2.keys())} #type: ignore
+  # yapf: enable
+  rx_traces:dict[str,list[tuple[int,bool]]]=defaultdict(list)
+
   count_low, count_high = 0, 0
-  for i in range(1000):
-    # refactored as list because apparently you can have multiple inputs with the same sender and receiver?
+  for i in range(iterations):
+    # refactored as list because apparently all the modules can process multiple inputs per cycle so using (sender,receiver) as dict key was breaking things
     inputs = [('button', 'broadcaster', LOW)]
     while inputs:
       count_low += len([x for x in inputs if x[2] == LOW])
@@ -69,6 +81,13 @@ def part1():
       for sender, receiver, state in inputs:
         # should use an enum but they're pretty annoying
         if DEBUG > 0: print(f'''{i=} {sender} -> {receiver}: {'HIGH' if state else 'LOW'}''')
+        # if rx_trap and receiver == 'rx':
+        #   print(f'{i=} {receiver=} {state=}')
+        #   # presumably this goes low every 10 trillion cycles and i'll need to look at what drives it and find the patterns in their cycles and do lcm. but lets pretend that's not true for now :)
+        #   if state == LOW: return i + 1
+        if rx_trap:
+          #BUG: this also needs to track the cycle num within each button press loop. in fact we might need to track the conjunctions and flip_flops dicts
+          rx_traces[sender].append((i, state))
         if receiver not in modules.keys(): continue
         module = modules[receiver]
         assert isinstance(module['targets'], list)  #should probably migrate to classes
@@ -99,12 +118,42 @@ def part1():
           next_inputs.extend([(receiver, x, state) for x in module['targets']])
       inputs = next_inputs
   product = count_low * count_high
-  print(f'part 1: {count_low=} {count_high=} {product=}')
+  if not rx_trap:
+    if DEBUG > 0: print(f'{count_low=} {count_high=} {product=}')
+    return product
+
+  cycles:dict[str,dict[str,int]]={}
+  for driver, values in rx_traces.items():
+    trues = sorted({i for i, s in values if s})
+    true_deltas = [y - x for x, y in zip(trues, trues[1:])]
+    falses = sorted({i for i, s in values if not s})
+    false_deltas = [y - x for x, y in zip(falses, falses[1:])]
+    # print(f'{driver=} {len(values)=} {trues[:4]=} {falses[:4]=}')
+    print(f'{driver=} {len(values)=} {true_deltas[:4]=} {false_deltas[:4]=}')
+    # drivers with irregular deltas will need to be simulated from their own drivers
+    # but i think there isn't enough data here yet - we need to know all of the states at each execution cycle for each button press cycle
+    if len({*true_deltas})!=1 or len({*false_deltas})!=1: continue
+    cycles[driver]={
+      'low_start':falses[0],
+      'low_interval':false_deltas[0],
+      'high_start':trues[0],
+      'high_interval':true_deltas[0],
+    }
+
+  print(f'{cycles=}')
+  return 0
+
+
+def part1():
+  result = solve()
+  print(f'part 1: {result}')
   # 883726240
 
 
 def part2():
-  ...
+  # need to go a bit higher to make sure we have enough values to validate consistent deltas for all the drivers
+  result = solve(100_000, True)
+  print(f'part 2: {result=}')
 
 
 if PART1: part1()
