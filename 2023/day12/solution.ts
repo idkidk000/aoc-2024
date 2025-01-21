669d5fc154540caa0b19641696e12a86bf830ef5
adminadmin <git@akki.uk>
2025-01-21 10:22:24 +0000

    2023 day 12: ts: refactor slightly

--- 2023/day12/solution.ts
+++ 2023/day12/solution.ts
@@ -2,11 +2,11 @@
 
 const DEBUG = Deno.args.reduce((acc, item) => (item.startsWith('-d') ? Number(item.slice(2) || '1') : acc), 0);
 const FILENAME = Deno.args.reduce(
-  (acc, item) => (item == '-i' ? 'input.txt' : item.startsWith('-e') ? `example${item.slice(2)}.txt` : acc),
+  (acc, item) => (item === '-i' ? 'input.txt' : item.startsWith('-e') ? `example${item.slice(2)}.txt` : acc),
   'example.txt'
 );
 const [PART1, PART2] = Deno.args.reduce(
-  (acc, item) => (item == '-p0' ? [false, false] : item == '-p1' ? [true, false] : item == '-p2' ? [false, true] : acc),
+  (acc, item) => (item === '-p0' ? [false, false] : item === '-p1' ? [true, false] : item === '-p2' ? [false, true] : acc),
   [true, true]
 );
 
@@ -19,8 +19,13 @@ const debug = (level: number, ...data: any[]) => {
 
 const input = await Deno.readTextFile(FILENAME);
 
+enum State {
+  Operational = '.',
+  Damaged = '#',
+  Indeterminate = '?',
+}
 interface SpringRecord {
-  data: string;
+  data: State[];
   checksums: number[];
 }
 
@@ -31,34 +36,30 @@ const parseInput = (): SpringRecord[] =>
     .map((line) => {
       const parts = line.split(/\s+/);
       return {
-        data: parts[0],
+        data: parts[0].split('') as State[],
         checksums: parts[1].split(',').map((token) => Number(token)),
       };
     });
 
 const countArrangements = (record: SpringRecord, cache: Map<string, number> = new Map()): number => {
-  // oh neat, the cache worked
-  const OPERATIONAL = '.';
-  const DAMAGED = '#';
-  const INDETERMINATE = '?';
-  const cacheKey = record.data + ' ' + record.checksums.join('');
+  const cacheKey = record.data.join('') + ' ' + record.checksums.join(',');
   if (cache.has(cacheKey)) return cache.get(cacheKey)!;
 
   debug(1, { cacheKey, record });
 
   // empty data && empty checksums: previous data was consumed by the previous checksum - return 1
   // empty data && non-empty checksums: impossible - return 0
-  if (record.data == '') {
+  if (record.data.length === 0) {
     debug(2, 'no data');
-    cache.set(cacheKey, record.checksums.length == 0 ? 1 : 0);
+    cache.set(cacheKey, record.checksums.length === 0 ? 1 : 0);
     return cache.get(cacheKey)!;
   }
 
   // empty checksums && damaged in data: impossible - return 0
   // empty checksums && damaged not in data: all remaining data must be undamaged - return 1
-  if (record.checksums.length == 0) {
+  if (record.checksums.length === 0) {
     debug(2, 'no checksums');
-    cache.set(cacheKey, record.data.includes(DAMAGED) ? 0 : 1);
+    cache.set(cacheKey, record.data.includes(State.Damaged) ? 0 : 1);
     return cache.get(cacheKey)!;
   }
 
@@ -66,15 +67,15 @@ const countArrangements = (record: SpringRecord, cache: Map<string, number> = ne
   let count = 0;
 
   // indeterminate matches both cases
-  if ([OPERATIONAL, INDETERMINATE].includes(record.data[0]))
+  if ([State.Operational, State.Indeterminate].includes(record.data[0]))
     count += countArrangements({ data: record.data.slice(1), checksums: record.checksums }, cache);
 
-  if ([DAMAGED, INDETERMINATE].includes(record.data[0])) {
+  if ([State.Damaged, State.Indeterminate].includes(record.data[0])) {
     if (record.checksums[0] > record.data.length) {
       debug(2, 'data too short to satisfy checksum');
-    } else if (record.data.slice(0, record.checksums[0]).includes(OPERATIONAL)) {
+    } else if (record.data.slice(0, record.checksums[0]).includes(State.Operational)) {
       debug(2, 'operational in checksummed data');
-    } else if (record.checksums[0] == record.data.length || record.data[record.checksums[0]] != DAMAGED) {
+    } else if (record.checksums[0] === record.data.length || record.data[record.checksums[0]] !== State.Damaged) {
       debug(2, 'valid');
       count += countArrangements(
         { data: record.data.slice(record.checksums[0] + 1), checksums: record.checksums.slice(1) },
@@ -102,12 +103,15 @@ const part2 = () => {
   const count = records
     .map((record) =>
       countArrangements({
-        data: Array(5).fill(record.data).join('?'),
+        data: Array(5)
+          .fill([...record.data, State.Indeterminate])
+          .flat()
+          .filter((_, i, arr) => i < arr.length - 1),
         checksums: Array(5).fill(record.checksums).flat(),
       })
     )
     .reduce((acc, item) => acc + item, 0);
-  console.log('part 1:', count);
+  console.log('part 2:', count);
 };
 
 if (PART1) part1();
