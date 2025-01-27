dd1136a05cfcbb5e800e311bc0ac8b6c99f6ef70
adminadmin <git@akki.uk>
2025-01-27 22:01:35 +0000

    2022 day 19 p1: ts: remove dead ends

--- 2022/day19/solution.ts
+++ 2022/day19/solution.ts
@@ -25,61 +25,6 @@ const debug = (level: number, ...data: any[]) => {
 const args = new Args();
 // #endregion
 
-class BinaryHeap<T> {
-  private heap: T[] = [];
-  constructor(public comparator: (a: T, b: T) => number) {}
-  get size(): number {
-    return this.heap.length;
-  }
-  push = (value: T) => {
-    this.heap.push(value);
-    this.siftUp();
-  };
-  pop = () => {
-    if (this.size === 0) return undefined;
-    const top = this.heap[0];
-    const end = this.heap.pop()!;
-    if (this.size > 0) {
-      this.heap[0] = end;
-      this.siftDown();
-    }
-    return top;
-  };
-  private siftUp = () => {
-    let idx = this.size - 1;
-    const element = this.heap[idx];
-    while (idx > 0) {
-      const parentIdx = Math.floor((idx - 1) / 2);
-      const parent = this.heap[parentIdx];
-      if (this.comparator(element, parent) >= 0) break;
-      this.heap[idx] = parent;
-      idx = parentIdx;
-    }
-    this.heap[idx] = element;
-  };
-  private siftDown = () => {
-    let idx = 0;
-    const length = this.size;
-    const element = this.heap[0];
-    while (true) {
-      const leftIdx = 2 * idx + 1;
-      const rightIdx = 2 * idx + 2;
-      let swapIdx = -1;
-
-      if (leftIdx < length && this.comparator(this.heap[leftIdx], element) < 0) {
-        swapIdx = leftIdx;
-      }
-      if (rightIdx < length && this.comparator(this.heap[rightIdx], this.heap[swapIdx === -1 ? idx : leftIdx]) < 0) {
-        swapIdx = rightIdx;
-      }
-      if (swapIdx === -1) break;
-      this.heap[idx] = this.heap[swapIdx];
-      idx = swapIdx;
-    }
-    this.heap[idx] = element;
-  };
-}
-
 type Material = 'ore' | 'clay' | 'obsidian' | 'geode';
 interface Blueprint {
   id: number;
@@ -103,107 +48,6 @@ const parseInput = () =>
       ),
     }))
     .toArray();
-
-const part1 = () => {
-  const blueprints = parseInput();
-  interface Inventory {
-    ore: number;
-    clay: number;
-    obsidian: number;
-    geode: number;
-  }
-  interface QueueEntry {
-    robots: Inventory;
-    resources: Inventory;
-    remaining: number;
-  }
-
-  const resultSummary = new Array<{ blueprintId: number; maxGeodes: number; qualityLevel: number }>();
-  for (const blueprint of blueprints) {
-    //TODO: can this be improved?
-    const queueRank = (item: QueueEntry) =>
-      (item.resources.geode + item.robots.geode * item.remaining) * 1000000 +
-      (item.resources.obsidian + item.robots.obsidian * item.remaining) * 10000 +
-      (item.resources.clay + item.robots.clay * item.remaining) * 100 +
-      (item.resources.ore + item.robots.ore * item.remaining);
-    const queue = new BinaryHeap<QueueEntry>((a, b) => queueRank(b) - queueRank(a));
-    const resourceLimits: Inventory = {
-      ore: blueprint.robots.values().reduce((acc, item) => Maths.max(acc, item.get('ore') ?? 0), 0),
-      clay: blueprint.robots.values().reduce((acc, item) => Maths.max(acc, item.get('clay') ?? 0), 0),
-      obsidian: blueprint.robots.values().reduce((acc, item) => Maths.max(acc, item.get('obsidian') ?? 0), 0),
-      geode: Infinity,
-    };
-    debug(1, { resourceLimits, blueprint });
-    queue.push({
-      robots: { ore: 1, clay: 0, obsidian: 0, geode: 0 },
-      resources: { ore: 0, clay: 0, obsidian: 0, geode: 0 },
-      remaining: 24,
-    });
-    let maxGeodes = 0;
-    let lastMax = performance.now();
-    while (queue.size > 0) {
-      const state = queue.pop()!;
-      debug(3, state);
-      if (state.remaining === 0) {
-        if (state.resources.geode >= maxGeodes) lastMax = performance.now();
-        if (state.resources.geode > maxGeodes) {
-          maxGeodes = state.resources.geode;
-          debug(2, { maxGeodes, state, qsize: queue.size });
-        }
-        continue;
-      }
-      if (
-        state.resources.ore > resourceLimits.ore * 3 ||
-        state.resources.clay > resourceLimits.clay * 3 ||
-        state.resources.geode > resourceLimits.geode * 3
-      )
-        continue;
-      //TODO: improve pruning a lot
-      // if (state.resources.ore > 50 || state.resources.clay > 50) continue;
-      // if (state.robots.geode === 0 && state.remaining < maxGeodes - 3) continue;
-      // if (state.robots.obsidian === 0 && state.remaining < blueprint.robots.get('geode')!.get('obsidian')!) continue;
-      if (lastMax > -1 && performance.now() - lastMax > 30_000) continue; //bail if we haven't set a new max in 10 sec
-      // if (state.resources.obsidian === 0 && blueprint.robots.get('geode')!.requires.get('obsidian')! > state.remaining) continue;
-      // if (state.remaining < 20 && state.robots.ore === 0) continue;
-      // if (state.remaining < 15 && state.robots.clay === 0) continue;
-      // if (state.remaining < 10 && state.robots.obsidian === 0) continue;
-      // // if (
-      // //   state.remaining < blueprint.robots.get('geode')!.requires.get('obsidian')! + maxGeodes - 2 &&
-      // //   state.robots.obsidian === 0
-      // // )
-      // //   continue;
-      // if (queue.size % 10000 === 0) debug(1, { qsize: queue.size });
-
-      for (const [robotType, robotRequirements] of blueprint.robots.entries()) {
-        const nextResources = { ...state.resources };
-        for (const [requiredResource, requiredResourceCount] of robotRequirements.entries())
-          nextResources[requiredResource] -= requiredResourceCount;
-        if (nextResources.clay < 0 || nextResources.geode < 0 || nextResources.obsidian < 0 || nextResources.ore < 0) continue;
-        for (const resourceType of blueprint.robots.keys()) nextResources[resourceType] += state.robots[resourceType];
-        queue.push({
-          robots: { ...state.robots, [robotType]: state.robots[robotType] + 1 },
-          resources: nextResources,
-          remaining: state.remaining - 1,
-        });
-      }
-
-      const nextResources = { ...state.resources };
-      for (const resourceType of blueprint.robots.keys()) nextResources[resourceType] += state.robots[resourceType];
-      queue.push({
-        robots: state.robots,
-        resources: nextResources,
-        remaining: state.remaining - 1,
-      });
-    }
-    resultSummary.push({ blueprintId: blueprint.id, maxGeodes, qualityLevel: blueprint.id * maxGeodes });
-    debug(1, { resultSummary, total: resultSummary.reduce((acc, item) => acc + item.qualityLevel, 0) });
-  }
-  const result = resultSummary.reduce((acc, item) => acc + item.qualityLevel, 0);
-  console.log('part 1:', result);
-
-  // 1183 too low ofc
-  // not 1283 either
-};
 
 const solve = (blueprint: Blueprint) => {
   interface Inventory {
@@ -272,10 +116,12 @@ const solve = (blueprint: Blueprint) => {
     cache.set(cacheKey, maxGeodes);
     return maxGeodes;
   };
-  return dfs({ clay: 0, geode: 0, obsidian: 0, ore: 0 }, { clay: 0, geode: 0, obsidian: 0, ore: 1 }, 24);
+  const result = dfs({ clay: 0, geode: 0, obsidian: 0, ore: 0 }, { clay: 0, geode: 0, obsidian: 0, ore: 1 }, 24);
+  debug(1, { blueprint, result });
+  return result;
 };
 
-const part1_2 = () => {
+const part1 = () => {
   const blueprints = parseInput();
   const resultsMap = new Map<number, number>(blueprints.map((item) => [item.id, solve(item)]));
   console.log(
@@ -286,5 +132,5 @@ const part1_2 = () => {
 
 const part2 = () => {};
 
-if (args.part1) part1_2();
+if (args.part1) part1();
 if (args.part2) part2();
